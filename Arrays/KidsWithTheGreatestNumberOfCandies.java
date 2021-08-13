1c86509102609c33e4720795259a18ff44707783
samueldenzil <denzil.s.samuel@gmail.com>
2021-08-13 23:29:00 +0530

    Added array program

---
+++ Arrays/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+        int max = candies[0];
+
+        for (int i = 1; i < candies.length; i++) {
+            if (max < candies[i]) {
+                max = candies[i];
+            }
+        }
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
