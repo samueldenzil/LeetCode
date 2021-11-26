8644faca927fd31d5c91a23cf47f1a2b444d6b2f
samueldenzil <denzil.s.samuel@gmail.com>
2021-11-26 17:54:28 +0530

    Add Searching Programs

---
+++ Searching/FindPeakElement.java
@@ -0,0 +1,26 @@
+// Google, Facebook Interview Question
+// https://leetcode.com/problems/find-peak-element/
+
+// This question is word to word same as "Peak Index in a Mountain Array"
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                // you are in the decreasing part of array
+                end = mid;
+            }
+            else if (nums[mid] < nums[mid + 1]) {
+                // you are in the ascending part of array
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
---
+++ Searching/FindTheRotationCountInRotatedSortedArray.java
@@ -0,0 +1,41 @@
+// https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
+
+public class FindTheRotationCountInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        int[] arr = {7, 9, 11, 12, 15};
+        int count = findPivotIndex(arr) + 1;
+        System.out.println(count);
+    }
+
+    public static int findPivotIndex(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            // case 1
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            // case 2
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            if (arr[start] >= arr[mid]) {
+                // case 3: so all the element after mid will be less than start element and the pivot element will be in the left half, so we can skip the right half
+                // and how we are sure that this mid is not the pivot because, if mid was the pivot it would have been returned in case 1 and 2
+                end = mid - 1;
+            }
+            else if (arr[start] < arr[mid]) {
+                // case 4: if start < mid then all the element between start and mid will be less than the pivot element, so we can find the pivot element in the right half
+                // and how we are sure that this mid is not the pivot because, if mid was the pivot it would have been returned in case 1 and 2
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
