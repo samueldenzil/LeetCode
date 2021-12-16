1bb8f917ddcbc5ef7928c1ac9b396430e93212f4
samueldenzil <denzil.s.samuel@gmail.com>
2021-12-16 17:12:16 +0530

    Add Recursion Program

---
+++ Recursion/RecursionDigitSumAndProduct.java
@@ -0,0 +1,26 @@
+
+public class RecursionDigitSumAndProduct {
+
+    public static void main(String[] args) {
+        int ans = product(505);
+
+        System.out.println(ans);
+    }
+
+    public static int sum(int n) {
+        // base case
+        if (n == 0) {
+            return 0;
+        }
+
+        return (n % 10) + sum(n / 10);
+    }
+
+    public static int product(int n) {
+        if (n % 10 == n) {
+            return n;
+        }
+
+        return (n % 10) * product(n / 10);
+    }
+}
