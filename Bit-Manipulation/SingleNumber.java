587e6a53c04c938c6ffd51f3af872afbe0271692
samueldenzil <denzil.s.samuel@gmail.com>
2022-01-07 17:42:09 +0530

    January 7, 2022

---
+++ Bit-Manipulation/SingleNumber.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/single-number/
+
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
