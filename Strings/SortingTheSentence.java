f5b47daee00292c1290e4769514c13874f4ddb14
samueldenzil <denzil.s.samuel@gmail.com>
2022-01-09 23:41:54 +0530

    January 9, 2022

---
+++ Strings/SortingTheSentence.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/sorting-the-sentence/
+
+class SortingTheSentence {
+    
+    public String sortSentence(String s) {
+        String[] strArr = s.split(" ");
+        int i = 0;
+
+        // applying cyclic sort
+        while (i < strArr.length) {
+            int intChar = (int) (strArr[i].charAt(strArr[i].length() - 1) - '0');
+            if (i != intChar - 1) {
+                String temp = strArr[i];
+                strArr[i] = strArr[intChar - 1];
+                strArr[intChar - 1] = temp;
+            } else {
+                i++;
+            }
+        }
+
+        // removing the last digit from the String
+        for (i = 0; i < strArr.length; i++) {
+            strArr[i] = strArr[i].substring(0, strArr[i].length() - 1);
+        }
+
+        // joining the array of strings to a String
+        return String.join(" ", strArr);
+    }
+}
