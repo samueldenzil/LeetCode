e720baa782e5e9509d4d30a9d3fe4c3dfb5ac73d
samueldenzil <denzil.s.samuel@gmail.com>
2022-02-05 00:07:21 +0530

    February 4, 2022

---
+++ All_Problems/IndexOfAnExtraElement.java
@@ -0,0 +1,41 @@
+// https://practice.geeksforgeeks.org/problems/index-of-an-extra-element/1
+package All_Problems;
+
+public class IndexOfAnExtraElement {
+
+    public static void main(String[] args) {
+        int[] a = {2,4,6,8,9,10,12};
+        int[] b = {2,4,6,8,10,12};
+        System.out.println(findExtra(a, b, a.length));
+    }
+
+    public static int findExtra(int[] a, int[] b, int n) {
+        // add code here.
+        int extraElement = 0;
+        for (int i = 0; i < b.length; i++) {
+            extraElement ^= a[i];
+            extraElement ^= b[i];
+        }
+        extraElement ^= a[n - 1];
+        return binarySearch(a, extraElement);
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Arrays/FourSumII.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/4sum-ii/
+package Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumII {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2};
+        int[] nums2 = {-2,-1};
+        int[] nums3 = {-1,2};
+        int[] nums4 = {0,2};
+        System.out.println(fourSumCount(nums1, nums2, nums3, nums4));
+    }
+
+    public static int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int a: nums1) {
+            for (int b: nums2) {
+                if (!map.containsKey(a + b)) {
+                    map.put(a + b, 1);
+                } else {
+                    int temp = map.get(a + b);
+                    map.put(a + b, temp + 1);
+                }
+            }
+        }
+
+        for (int c: nums3) {
+            for (int d: nums4) {
+                if (map.containsKey(-(c + d))) {
+                    ans += map.get(-(c + d));
+                }
+            }
+        }
+
+        return ans;
+    }
+}
