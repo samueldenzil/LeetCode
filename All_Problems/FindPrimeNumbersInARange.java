fa4ff3350dcad4aeba2fa6dc0ea822c687ba9a19
samueldenzil <denzil.s.samuel@gmail.com>
2022-02-09 01:08:18 +0530

    February 8, 2022

---
+++ All_Problems/FindPrimeNumbersInARange.java
@@ -0,0 +1,37 @@
+// https://practice.geeksforgeeks.org/problems/find-prime-numbers-in-a-range4718/1
+package All_Problems;
+
+import java.util.ArrayList;
+
+public class FindPrimeNumbersInARange {
+
+    public static void main(String[] args) {
+        int M = 1;
+        int N = 10;
+        System.out.println(primeRange(M, N));
+    }
+
+    static ArrayList<Integer> primeRange(int M, int N) {
+        // code here
+        ArrayList<Integer> primeNums = new ArrayList<>();
+
+        for (int i = M; i <= N; i++) {
+            if (isPrimeNumber(i)) {
+                primeNums.add(i);
+            }
+        }
+
+        return primeNums;
+    }
+
+    static boolean isPrimeNumber(int n) {
+        if (n == 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Arrays/RotateArray.java
@@ -0,0 +1,39 @@
+// https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1
+package Arrays;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int N = arr.length;
+        int D = 2;
+
+        rotateArr(arr, D, N);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int[] arr, int d, int n) {
+        // add your code here
+        // reverse the entire array
+        reverseArray(arr, 0, n - 1);
+
+        // reverse the array from 0 to n - d - 1
+        reverseArray(arr, 0, n - d - 1);
+
+        // reverse the remaining part of the array
+        reverseArray(arr, n - d, n - 1);
+    }
+
+    static void reverseArray(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
