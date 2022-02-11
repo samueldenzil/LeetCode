0495858c8041b50b55a68c7da1cb10ca0c982fbe
samueldenzil <denzil.s.samuel@gmail.com>
2022-02-12 00:06:47 +0530

    February 11, 2022

---
+++ All_Problems/RemoveLeadingZerosFromAnIPAddress.java
@@ -0,0 +1,44 @@
+// https://practice.geeksforgeeks.org/problems/remove-leading-zeros-from-an-ip-address3530/1
+
+package All_Problems;
+
+public class RemoveLeadingZerosFromAnIPAddress {
+
+    public static void main(String[] args) {
+        String S = "100.020.003.400";
+        System.out.println(newIPAdd(S));
+    }
+
+    public static String newIPAdd(String S) {
+        // your code here
+        StringBuilder str = new StringBuilder();
+
+        int i = 0;
+        int prev = 0;
+        while (i < S.length()) {
+            if (S.charAt(i) == '.') {
+                while (S.charAt(prev) == '0') {
+                    prev++;
+                }
+                if (prev == i)
+                    str.append('0');
+                else
+                    str.append(S, prev, i);
+                str.append('.');
+                prev = i + 1;
+            }
+            i++;
+        }
+
+        while (prev < S.length() && S.charAt(prev) == '0') {
+            prev++;
+        }
+
+        if (prev == i)
+            str.append('0');
+        else
+            str.append(S, prev, S.length());
+
+        return str.toString();
+    }
+}
