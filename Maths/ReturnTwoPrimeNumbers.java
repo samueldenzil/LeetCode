c1f1fbc594367d5e530aafe99ebdefff8b467aaa
samueldenzil <denzil.s.samuel@gmail.com>
2022-02-25 00:05:21 +0530

    February 24, 2022

---
+++ Maths/ReturnTwoPrimeNumbers.java
@@ -0,0 +1,40 @@
+// https://practice.geeksforgeeks.org/problems/return-two-prime-numbers2509/1
+package Maths;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReturnTwoPrimeNumbers {
+
+    public static void main(String[] args) {
+        int N = 74;
+        System.out.println(primeDivision(N));
+    }
+
+    static List<Integer> primeDivision(int N){
+        // code here
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 2; i <= N / 2; i++) {
+            if (isPrime(i) && isPrime(N - i)) {
+                ans.add(i);
+                ans.add(N - i);
+
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
---
+++ Strings/ExcelSheetColumnNumber.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/excel-sheet-column-number/
+package Strings;
+
+public class ExcelSheetColumnNumber {
+
+    public static void main(String[] args) {
+        String columnTitle = "AB";
+        System.out.println(titleToNumber(columnTitle));
+    }
+
+    public static int titleToNumber(String columnTitle) {
+        int columnNumber = 0;
+
+        int power = 0;
+        for (int i = columnTitle.length() - 1; i >= 0; i--) {
+            char c = columnTitle.charAt(i);
+
+            columnNumber += (c - 'A' + 1) * Math.pow(26, power);
+            power++;
+        }
+
+        return columnNumber;
+    }
+}
