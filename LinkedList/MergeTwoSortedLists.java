56baa572d88cdaa4c376b985b17bf10984aac911
samueldenzil <denzil.s.samuel@gmail.com>
2022-03-09 21:47:34 +0530

    Update linkedlist program

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -2,23 +2,6 @@
 
 package LinkedList;
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
