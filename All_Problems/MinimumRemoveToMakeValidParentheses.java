b627bcc377ae286256dc1dd6bdbd9d904fc2153a
samueldenzil <denzil.s.samuel@gmail.com>
2022-03-16 00:12:12 +0530

    March 15, 2022

---
+++ All_Problems/MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+package All_Problems;
+
+public class MinimumRemoveToMakeValidParentheses {
+
+    public static void main(String[] args) {
+        String s = "lee(t(c)o)de)";
+        System.out.println(minRemoveToMakeValid(s));
+    }
+
+    public static String minRemoveToMakeValid(String s) {
+        char[] letters = s.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == '(') {
+                count++;
+            } else if (letters[i] == ')') {
+                if (count > 0) {
+                    count--;
+                } else {
+                    letters[i] = 0;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = letters.length - 1; i >= 0; i--) {
+            if (letters[i] == ')') {
+                count++;
+            } else if (letters[i] == '(') {
+                if (count > 0) {
+                    count--;
+                } else {
+                    letters[i] = 0;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : letters) {
+            if (c != 0) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
