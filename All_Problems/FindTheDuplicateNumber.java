e5fbc0ed8251dca7fe9dde2db468c916ca9c153f
samueldenzil <denzil.s.samuel@gmail.com>
2022-03-30 00:40:42 +0530

    March 29, 2022

--- All_Problems/FindTheDuplicateNumber.java
+++ All_Problems/FindTheDuplicateNumber.java
@@ -1,30 +1,31 @@
-package All_Problems;
 // Microsoft Interview Question
 // https://leetcode.com/problems/find-the-duplicate-number/
+
+package All_Problems;
 
 class FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
         int i = 0;
-        
-        while (i < nums.length) {            
+
+        while (i < nums.length) {
             int correct = nums[i] - 1;
-            
+
             if (nums[i] != nums[correct]) {
                 swap(nums, i, correct);
             } else {
                 i++;
             }
         }
-        
+
         for (i = 0; i < nums.length; i++) {
-            
+
             if (nums[i] - 1 != i) {
                 return nums[i];
             }
         }
         return nums.length;
     }
-    
+
     public void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
--- Sorting/FindTheDuplicateNumber.java
+++ Sorting/FindTheDuplicateNumber.java
@@ -1,30 +1,30 @@
-package Sorting;
 // Microsoft Interview Question
 // https://leetcode.com/problems/find-the-duplicate-number/
+package Sorting;
 
 class FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
         int i = 0;
-        
-        while (i < nums.length) {            
+
+        while (i < nums.length) {
             int correct = nums[i] - 1;
-            
+
             if (nums[i] != nums[correct]) {
                 swap(nums, i, correct);
             } else {
                 i++;
             }
         }
-        
+
         for (i = 0; i < nums.length; i++) {
-            
+
             if (nums[i] - 1 != i) {
                 return nums[i];
             }
         }
         return nums.length;
     }
-    
+
     public void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
