86eb9ed1b4e91eb4f652a2a451adc96c19e27ad5
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-02 00:23:03 +0530

    April 1, 2022

---
+++ Strings/ReverseString.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/reverse-string/
+package Strings;
+
+import java.util.Arrays;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+
+    public static void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while (start <= end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
