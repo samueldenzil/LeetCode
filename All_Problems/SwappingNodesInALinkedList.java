f06dfcf0c82948f43ad25bff21823125c0560a7d
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-05 21:17:05 +0530

    April 4, 2022

---
+++ All_Problems/SwappingNodesInALinkedList.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+package All_Problems;
+
+public class SwappingNodesInALinkedList {
+
+    public static ListNode swapNodes(ListNode head, int k) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+
+        int i = 1;
+        while (i != k) {
+            p1 = p1.next;
+            i++;
+        }
+
+        ListNode nthNode = p1;
+
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        int temp = nthNode.val;
+        nthNode.val = p2.val;
+        p2.val = temp;
+
+        return head;
+    }
+}
