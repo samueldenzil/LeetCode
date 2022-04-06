f1c39786206d673dbd359d545affab1705066b6e
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-06 17:41:33 +0530

    April 5, 2022

---
+++ Arrays/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/container-with-most-water/
+package Arrays;
+
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height));
+    }
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+
+        int area = 0;
+
+        while (left <= right) {
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int tempArea = h * w;
+            area = Math.max(area, tempArea);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return area;
+    }
+}
