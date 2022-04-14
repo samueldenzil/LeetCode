c3899f26f3940e14d8ebf57f8c6621221d0cc937
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-14 10:25:52 +0530

    April 13, 2022

---
+++ All_Problems/SpiralMatrixII.java
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/spiral-matrix-ii/
+package All_Problems;
+
+import java.util.Arrays;
+
+public class SpiralMatrixII {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] matrix = generateMatrix(n);
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static int[][] generateMatrix(int n) {
+        int[][] ans = new int[n][n];
+        int counter = 1;
+
+        // Initially we set 4 pointers left, right, top, down as 0, n - 1, 0, n - 1
+        // respectively
+
+        int left = 0, right = n - 1;
+        int top = 0, down = n - 1;
+
+        // 0 = left -> right
+        // 1 = top -> down
+        // 2 = right -> left
+        // 3 = down -> top
+
+        int direction = 0; // Initially we are staring from left to right
+
+        while (left <= right && top <= down) {
+            if (direction == 0) {
+                // Going from left to right
+                for (int i = left; i <= right; i++) {
+                    ans[top][i] = counter++;
+                }
+                // After going from left to right I want to go one step down
+                top += 1;
+            } else if (direction == 1) {
+                // Going from top to down
+                for (int i = top; i <= down; i++) {
+                    ans[i][right] = counter++;
+                }
+                // After going from top to down I want to go one step left
+                right -= 1;
+            } else if (direction == 2) {
+                // Going from right to left
+                for (int i = right; i >= left; i--) {
+                    ans[down][i] = counter++;
+                }
+                // After going from right to left I want to go one step top
+                down -= 1;
+            } else {
+                // Going from down to top
+                for (int i = down; i >= top; i--) {
+                    ans[i][left] = counter++;
+                }
+                // After going from down to top I want to go one step right
+                left += 1;
+            }
+            // direction = (direction + 1) % 4;
+            direction += 1;
+            direction %= 4;
+        }
+
+        return ans;
+    }
+}
---
+++ LinkedList/BrowserHistory.java
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/design-browser-history/
+package LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowserHistory {
+
+    List<String> stack;
+    int pointerIndex = 0;
+
+    public BrowserHistory(String homepage) {
+        stack = new ArrayList<>();
+        stack.add(pointerIndex, homepage);
+    }
+
+    public void visit(String url) {
+        pointerIndex += 1;
+        stack.add(pointerIndex, url);
+        int temp = pointerIndex;
+        while (temp < stack.size() - 1) {
+            stack.remove(temp + 1);
+        }
+        pointerIndex = stack.size() - 1;
+    }
+
+    public String back(int steps) {
+        pointerIndex -= steps;
+        if (pointerIndex < 0) {
+            pointerIndex = 0;
+        }
+        return stack.get(pointerIndex);
+    }
+
+    public String forward(int steps) {
+        pointerIndex += steps;
+        if (pointerIndex >= stack.size()) {
+            pointerIndex = stack.size() - 1;
+        }
+        return stack.get(pointerIndex);
+    }
+
+    public static void main(String[] args) {
+        BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+        // You are in "leetcode.com". Visit "google.com"
+        browserHistory.visit("google.com");
+        // You are in "google.com". Visit "facebook.com"
+        browserHistory.visit("facebook.com");
+        // You are in "facebook.com". Visit "youtube.com"
+        browserHistory.visit("youtube.com");
+        // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+        System.out.println(browserHistory.back(1));
+        // You are in "facebook.com", move back to "google.com" return "google.com"
+        System.out.println(browserHistory.back(1));
+        // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+        System.out.println(browserHistory.forward(1));
+        // You are in "facebook.com". Visit "linkedin.com"
+        browserHistory.visit("linkedin.com");
+        // You are in "linkedin.com", you cannot move forward any steps.
+        System.out.println(browserHistory.forward(2));
+        // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+        System.out.println(browserHistory.back(2));
+        // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+        System.out.println(browserHistory.back(7));
+    }
+}
