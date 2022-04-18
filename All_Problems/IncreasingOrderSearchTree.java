ef6b186fb8a9dabf5e74d6f0c079957373f9fef3
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-18 18:32:12 +0530

    April 17, 2022

---
+++ All_Problems/IncreasingOrderSearchTree.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/increasing-order-search-tree/
+package All_Problems;
+
+public class IncreasingOrderSearchTree {
+
+    TreeNode temp = new TreeNode(-1);
+    TreeNode dummy = temp;
+
+    public TreeNode increasingBST(TreeNode root) {
+        inorder(root);
+        return dummy.right;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        temp.right = root;
+        root.left = null;
+        temp = temp.right;
+        inorder(root.right);
+    }
+}
