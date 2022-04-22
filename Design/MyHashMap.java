674bc94740163c7bb4139c3200fed5387f659376
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-23 00:47:55 +0530

    April 22, 2022

---
+++ Design/MyHashMap.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/design-hashmap/
+package Design;
+
+import java.util.Arrays;
+
+class MyHashMap {
+
+    int[] map = null;
+
+    public MyHashMap() {
+        map = new int[1000001];
+        Arrays.fill(map, -1);
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
+    public int get(int key) {
+        return map[key];
+    }
+
+    public void remove(int key) {
+        put(key, -1);
+    }
+
+    public static void main(String[] args) {
+        MyHashMap myHashMap = new MyHashMap();
+        myHashMap.put(1, 1); // The map is now [[1,1]]
+        myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+        System.out.println(myHashMap.get(1));    // return 1, The map is now [[1,1], [2,2]]
+        System.out.println(myHashMap.get(3));    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+        myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+        System.out.println(myHashMap.get(2));    // return 1, The map is now [[1,1], [2,1]]
+        myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+        System.out.println(myHashMap.get(2));    // return -1 (i.e., not found), The map is now [[1,1]]
+    }
+}
