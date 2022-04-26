b7598ee09728c3d9dfe9372cb245c1b46f896665
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-26 21:04:29 +0530

    April 25, 2022

---
+++ Design/PeekingIterator.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/peeking-iterator/
+package Design;
+
+import java.util.Iterator;
+
+public class PeekingIterator implements Iterator<Integer> {
+    // Peek element
+    Integer nextEl = null;
+    Iterator<Integer> it = null;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        this.it = iterator;
+        nextEl = it.next();
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return nextEl;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer getElement = nextEl;
+        if (it.hasNext()) {
+            nextEl = it.next();
+        } else {
+            nextEl = null;
+        }
+
+        return getElement;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextEl != null;
+    }
+}
