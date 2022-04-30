4e62c09f5ad3d55f4984af0be2e5dbee934543cd
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-30 22:01:38 +0530

    April 29, 2022

---
+++ All_Problems/CountPrefixesOfAGivenString.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/count-prefixes-of-a-given-string/
+package All_Problems;
+
+public class CountPrefixesOfAGivenString {
+
+    public static void main(String[] args) {
+        String[] words = {"a", "b", "c", "ab", "bc", "abc"};
+        String s = "abc";
+        System.out.println(countPrefixes(words, s));
+    }
+
+    public static int countPrefixes(String[] words, String s) {
+        int count = 0;
+
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
