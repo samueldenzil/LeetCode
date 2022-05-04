dece1c8fc690f8df6914e34d492942cbfce8c5fb
samueldenzil <denzil.s.samuel@gmail.com>
2022-05-04 21:52:19 +0530

    May 3, 2022

---
+++ All_Problems/MaxNumberOfKSumPairs.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/max-number-of-k-sum-pairs/
+package All_Problems;
+
+import java.util.Arrays;
+
+public class MaxNumberOfKSumPairs {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 3, 4, 3};
+        int k = 5;
+        System.out.println(maxOperations(nums, k));
+    }
+
+    // TC: O(nlog(n)) SC: O(1)
+    public static int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length - 1;
+
+        int count = 0;
+        while (start < end) {
+            if (nums[start] == k - nums[end]) { // nums[start] + nums[end] == k
+                count++;
+                start++;
+                end--;
+            } else if (nums[start] > k - nums[end]) {  // nums[start] + nums[end] > k
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return count;
+    }
+}
