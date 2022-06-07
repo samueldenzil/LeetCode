a040732177152991f358b50c5ad4fe59d7776236
samueldenzil <denzil.s.samuel@gmail.com>
2022-06-07 09:34:35 +0530

    June 6, 2022

---
+++ Dynamic_Programming/PartitionEqualSubsetSum.java
@@ -0,0 +1,137 @@
+// https://www.codingninjas.com/codestudio/problems/partition-equal-subset-sum_892980
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class PartitionEqualSubsetSum {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 1, 2, 2, 1};
+        int n = arr.length;
+        System.out.println(canPartition(arr, n));
+    }
+
+    public static boolean canPartition(int[] arr, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        if (sum % 2 == 1) {
+            return false;
+        }
+
+        int k = sum / 2;
+
+        boolean[] prev = new boolean[k + 1];
+
+        // Base case
+        prev[0] = true;
+        if (arr[0] < k) {
+            prev[arr[0]] = true;
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            boolean[] curr = new boolean[k + 1];
+            for (int target = 1; target < k + 1; target++) {
+                boolean take = false;
+                if (target >= arr[ind]) {
+                    take = prev[target - arr[ind]];
+                }
+                boolean notTake = prev[target];
+
+                curr[target] = take || notTake;
+            }
+            prev = curr;
+        }
+
+        return prev[k];
+    }
+
+
+    // Tabulation
+    /* public static boolean canPartition(int[] arr, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        if (sum % 2 == 1) {
+            return false;
+        }
+
+        int k = sum / 2;
+
+        boolean[][] dp = new boolean[n][k + 1];
+
+        // Base case
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if (arr[0] < k) {
+            dp[0][arr[0]] = true;
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target < k + 1; target++) {
+                boolean take = false;
+                if (target >= arr[ind]) {
+                    take = dp[ind - 1][target - arr[ind]];
+                }
+                boolean notTake = dp[ind - 1][target];
+
+                dp[ind][target] = take || notTake;
+            }
+        }
+
+        return dp[n - 1][k];
+    } */
+
+
+    // Memoization
+    /* public static boolean canPartition(int[] arr, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        if (sum % 2 == 1) {
+            return false;
+        }
+
+        int target = sum / 2;
+
+        int[][] dp = new int[n][target + 1];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return helper(n - 1, target, arr, dp);
+    }
+
+    private static boolean helper(int ind, int target, int[] arr, int[][] dp) {
+        // Base case
+        if (target == 0) {
+            return true;
+        }
+        if (ind == 0) {
+            return arr[0] == target;
+        }
+
+        if (dp[ind][target] != -1) {
+            return dp[ind][target] == 1;
+        }
+
+        boolean take = false;
+        if (arr[ind] < target) {
+            take = helper(ind - 1, target - arr[ind], arr, dp);
+        }
+        boolean notTake = helper(ind - 1, target, arr, dp);
+
+        boolean result = take || notTake;
+        dp[ind][target] = result ? 1 : 0;
+
+        return result;
+    } */
+}
---
+++ LinkedList/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,77 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists
+package LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectionOfTwoLinkedLists {
+
+    private static void generateLinkedList(ListNode head, int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            head.next = new ListNode(arr[i]);
+            head = head.next;
+        }
+    }
+
+    // TC: O(n + m)
+    // SC: O(1)
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        int len1 = 0;
+        int len2 = 0;
+
+        while (tempA != null) {
+            len1++;
+            tempA = tempA.next;
+        }
+
+        while (tempB != null) {
+            len2++;
+            tempB = tempB.next;
+        }
+
+        tempA = headA;
+        tempB = headB;
+        while (len1 > len2) {
+            tempA = tempA.next;
+            len1--;
+        }
+        while (len1 < len2) {
+            tempB = tempB.next;
+            len2--;
+        }
+
+        while (tempA != tempB) {
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+
+        return tempA;
+    }
+
+
+    // TC: O(n + m)
+    // SC: O(n)
+    /* public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        List<ListNode> list = new ArrayList<>();
+
+        while (tempA != null) {
+            list.add(tempA);
+            tempA = tempA.next;
+        }
+
+        while (tempB != null) {
+            if (list.contains(tempB)) {
+                return tempB;
+            }
+            tempB = tempB.next;
+        }
+
+        return null;
+    } */
+}
