756d7d1258d2fe3ac3ed9d271a80a4f600c1ba5e
samueldenzil <denzil.s.samuel@gmail.com>
2022-06-09 07:53:09 +0530

    June 8, 2022

---
+++ All_Problems/MaximumSubarray.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/maximum-subarray/
+package All_Problems;
+
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ Arrays/NextPermutation.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/next-permutation/
+package Arrays;
+
+import java.util.Arrays;
+
+public class NextPermutation {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 4, 2};
+        nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        int ind1 = i;
+
+        i = nums.length - 1;
+        if (ind1 >= 0) {
+            while (nums[i] <= nums[ind1]) {
+                i--;
+            }
+            swap(nums, ind1, i);
+        }
+        rotate(nums, ind1 + 1);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static void rotate(int[] arr, int start) {
+        int i = start;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+    }
+}
