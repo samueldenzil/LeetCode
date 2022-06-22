860b94960919327092edb5c75780a5044c619bcb
samueldenzil <denzil.s.samuel@gmail.com>
2022-06-22 08:51:32 +0530

    June 21, 2022

---
+++ All_Problems/DeleteAndEarn.java
@@ -0,0 +1,113 @@
+// https://leetcode.com/problems/delete-and-earn/
+package All_Problems;
+
+public class DeleteAndEarn {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 2};
+        System.out.println(deleteAndEarn(nums));
+    }
+
+    // TC: O(n) + O(n)
+    // SC: O(1)
+    public static int deleteAndEarn(int[] nums) {
+        int n = 10001;
+        int[] points = new int[n];
+
+        int lastIndex = -1;
+
+        for (int num : nums) {
+            points[num]++;
+            lastIndex = Math.max(lastIndex, num);
+        }
+
+        // base case
+        int prev = points[0];
+        int prev2 = 0;
+
+        for (int ind = 1; ind < lastIndex + 1; ind++) {
+            int take = points[ind] * ind;
+            if (ind - 2 >= 0) {
+                take += prev2;
+            }
+            int notTake = 0 + prev;
+
+            int curr = Math.max(take, notTake);
+
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
+
+    
+    // TC: O(n) + O(n)
+    // SC: O(n)
+    /* public static int deleteAndEarn(int[] nums) {
+        int n = 10001;
+        int[] points = new int[n];
+
+        int lastIndex = -1;
+
+        for (int num : nums) {
+            points[num]++;
+            lastIndex = Math.max(lastIndex, num);
+        }
+
+        int[] dp = new int[lastIndex + 1];
+
+        // base case
+        dp[0] = points[0];
+
+        for (int ind = 1; ind < lastIndex + 1; ind++) {
+            int take = points[ind] * ind;
+            if (ind - 2 >= 0) {
+                take += dp[ind - 2];
+            }
+            int notTake = 0 + dp[ind - 1];
+
+            dp[ind] = Math.max(take, notTake);
+        }
+
+        return dp[lastIndex];
+    } */
+
+
+    // TC: O(n) + O(n)
+    // SC: O(n) + O(n)
+    /* public static int deleteAndEarn(int[] nums) {
+        int n = 10001;
+        int[] points = new int[n];
+
+        int lastIndex = -1;
+
+        for (int num : nums) {
+            points[num]++;
+            lastIndex = Math.max(lastIndex, num);
+        }
+
+        int[] dp = new int[lastIndex + 1];
+        Arrays.fill(dp, -1);
+
+        return helper(lastIndex, points, dp);
+    }
+
+    private static int helper(int ind, int[] points, int[] dp) {
+        if (ind == 0) {
+            return points[0];
+        }
+        if (ind < 0) {
+            return 0;
+        }
+
+        if (dp[ind] != -1) {
+            return dp[ind];
+        }
+
+        int take = (points[ind] * ind) + helper(ind - 2, points, dp);
+        int notTake = 0 + helper(ind - 1, points, dp);
+
+        return dp[ind] = Math.max(take, notTake);
+    } */
+}
---
+++ Dynamic_Programming/JumpGame.java
@@ -0,0 +1,92 @@
+// https://leetcode.com/problems/jump-game/
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class JumpGame {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(canJump(nums));
+    }
+
+    // Tabulation
+    public static boolean canJump(int[] nums) {
+        int n = nums.length;
+
+        boolean[] dp = new boolean[n];
+
+        // base case
+        dp[n - 1] = true;
+
+        for (int ind = n - 2; ind >= 0; ind--) {
+            int allowedJumps = nums[ind];
+
+            boolean flag = false;
+
+            for (int i = allowedJumps; i >= 1; i--) {
+                if (isValidJump(ind, i, n)) {
+                    boolean curr = dp[ind + i];
+                    if (curr) {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+
+            dp[ind] = flag;
+        }
+
+        return dp[0];
+    }
+
+    private static boolean isValidJump(int currIndex, int step, int n) {
+        if (currIndex + step < n) {
+            return true;
+        }
+        return false;
+    }
+
+
+    // Memoization
+    /* public static boolean canJump(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return helper(0, nums, n, dp);
+    }
+
+    private static boolean helper(int ind, int[] nums, int n, int[] dp) {
+        // base case
+        if (ind == n - 1) {
+            return true;
+        }
+
+        if (dp[ind] != -1) {
+            return dp[ind] == 1;
+        }
+
+        int allowedJumps = nums[ind];
+        for (int i = allowedJumps; i >= 1; i--) {
+            if (isValidJump(ind, i, n)) {
+                boolean curr = helper(ind + i, nums, n, dp);
+                if (curr) {
+                    dp[ind] = 1;
+                    return true;
+                }
+            }
+        }
+
+        dp[ind] = 0;
+        return false;
+    }
+
+    private static boolean isValidJump(int currIndex, int step, int n) {
+        if (currIndex + step < n) {
+            return true;
+        }
+        return false;
+    } */
+}
