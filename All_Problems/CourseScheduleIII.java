b5bd16603380dd449b8bf76149288f6277e21b6e
samueldenzil <denzil.s.samuel@gmail.com>
2022-06-24 08:54:53 +0530

    June 23, 2022

---
+++ All_Problems/CourseScheduleIII.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/course-schedule-iii/
+package All_Problems;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class CourseScheduleIII {
+
+    public static void main(String[] args) {
+        int[][] courses = {{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}};
+        System.out.println(scheduleCourse(courses));
+    }
+
+    public static int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> (a[1] - b[1]));
+
+        // Max heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+
+        int day = 0;
+        for (int[] course : courses) {
+            int duration = course[0];
+            int lastDay = course[1];
+
+            if (day + duration <= lastDay) {
+                pq.add(duration);
+                day += duration;
+            } else if (!pq.isEmpty() && pq.peek() > duration) {
+                day -= pq.poll();
+                pq.add(duration);
+                day += duration;
+            } else {
+                // reject the current course
+            }
+        }
+
+        return pq.size();
+    }
+}
---
+++ Dynamic_Programming/CountPossibleWaysToConstructBuildings.java
@@ -0,0 +1,113 @@
+// https://practice.geeksforgeeks.org/problems/count-possible-ways-to-construct-buildings5007/1
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class CountPossibleWaysToConstructBuildings {
+
+    public static void main(String[] args) {
+        int N = 3;
+        System.out.println(TotalWays(N));
+    }
+
+    static int MOD = (int) (1e9 + 7);
+
+    // TC: O(n * 2)
+    // SC: O(2 * 2)
+    public static int TotalWays(int N) {
+        // Code here
+        long[] prev = new long[2];
+
+        // base case
+        prev[0] = 1;
+        prev[1] = 1;
+
+        for (int ind = 1; ind <= N; ind++) {
+            long[] curr = new long[2];
+            for (int prevPlot = 0; prevPlot <= 1; prevPlot++) {
+                if (prevPlot == 1) {
+                    // if we have already built the building we cannot use this place now
+                    curr[prevPlot] = (int) (prev[0] % MOD);
+                    continue;
+                }
+
+                // if the prev plot is empty then we have to option to build the building or leave the place
+                int take = (int) prev[1];
+                int notTake = (int) prev[0];
+
+                curr[prevPlot] = (take + notTake) % MOD;
+            }
+            prev = curr;
+        }
+
+        long ans = prev[0];
+        return (int) ((ans * ans) % MOD);
+    }
+
+
+    // TC: O(n * 2)
+    // SC: O(n * 2)
+    /* public static int TotalWays(int N) {
+        // Code here
+        long[][] dp = new long[N + 1][2];
+
+        // base case
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+
+        for (int ind = 1; ind <= N; ind++) {
+            for (int prevPlot = 0; prevPlot <= 1; prevPlot++) {
+                if (prevPlot == 1) {
+                    // if we have already built the building we cannot use this place now
+                    dp[ind][prevPlot] = (int) (dp[ind - 1][0] % MOD);
+                    continue;
+                }
+
+                // if the prev plot is empty then we have to option to build the building or leave the place
+                int take = (int) dp[ind - 1][1];
+                int notTake = (int) dp[ind - 1][0];
+
+                dp[ind][prevPlot] = (take + notTake) % MOD;
+            }
+        }
+
+        long ans = dp[N][0];
+        return (int) ((ans * ans) % MOD);
+    } */
+
+
+    // TC: O(N * 2)
+    // SC: O(n * 2) + O(n)
+    /* public static int TotalWays(int N) {
+        // Code here
+        int[][] dp = new int[N + 1][2];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        long ans = helper(N, 0, dp);
+        return (int) ((ans * ans) % MOD);
+    }
+
+    private static long helper(int ind, int prevPlot, int[][] dp) {
+        // base case
+        if (ind == 0) {
+            return 1;
+        }
+
+        if (dp[ind][prevPlot] != -1) {
+            return dp[ind][prevPlot];
+        }
+
+        if (prevPlot == 1) {
+            // if we have already built the building we cannot use this place now
+            return dp[ind][prevPlot] = (int) helper(ind - 1, 0, dp);
+        }
+
+        // if the prev plot is empty then we have to option to build the building or leave the place
+        int take = (int) helper(ind - 1, 1, dp);
+        int notTake = (int) helper(ind - 1, 0, dp);
+
+        return dp[ind][prevPlot] = (take + notTake) % MOD;
+    } */
+}
