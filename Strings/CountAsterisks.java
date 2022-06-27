22d55c92e06bf44e95af8ac3e4387ae425386362
samueldenzil <denzil.s.samuel@gmail.com>
2022-06-27 08:27:13 +0530

    June 26, 2022

---
+++ Strings/CountAsterisks.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/count-asterisks/
+package Strings;
+
+public class CountAsterisks {
+
+    public static void main(String[] args) {
+        String s = "l|*e*et|c**o|*de|";
+        System.out.println(countAsterisks(s));
+    }
+
+    public static int countAsterisks(String s) {
+        int count = 0;
+        boolean insidePair = false;
+
+        for (char c : s.toCharArray()) {
+            if (c == '|') {
+                if (insidePair) {
+                    insidePair = false;
+                } else {
+                    insidePair = true;
+                }
+            } else if (c == '*') {
+                if (!insidePair) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
