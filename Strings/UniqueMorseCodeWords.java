f77540d570f287fd957cebc7359de300a2c5e9f2
samueldenzil <denzil.s.samuel@gmail.com>
2022-06-30 09:09:50 +0530

    June 29, 2022

---
+++ Strings/UniqueMorseCodeWords.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/unique-morse-code-words/
+package Strings;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class UniqueMorseCodeWords {
+
+    public static void main(String[] args) {
+        String[] words = {"gin", "zen", "gig", "msg"};
+        System.out.println(uniqueMorseRepresentations(words));
+    }
+
+    public static int uniqueMorseRepresentations(String[] words) {
+        String[] morseTable = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        Set<String> set = new HashSet<>();
+
+        for (String word : words) {
+            String code = getMorseCode(word, morseTable);
+            set.add(code);
+        }
+
+        return set.size();
+    }
+
+    private static String getMorseCode(String word, String[] morseTable) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            sb.append(morseTable[index]);
+        }
+        return sb.toString();
+    }
+}
