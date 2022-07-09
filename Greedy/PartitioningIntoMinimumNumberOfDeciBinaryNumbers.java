c8da008e67721f63a5df58c9c5fec5b472ea65c1
samueldenzil <denzil.s.samuel@gmail.com>
2022-07-09 18:27:04 +0530

    July 8, 2022

---
+++ Greedy/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+package Greedy;
+
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+
+    public static void main(String[] args) {
+        String n = "32";
+        System.out.println(minPartitions(n));
+    }
+
+    public static int minPartitions(String n) {
+        int max = 0;
+
+        for (int i = 0; i < n.length(); i++) {
+            int digit = n.charAt(i) - '0';
+            max = Math.max(max, digit);
+            if (max == 9) {
+                break;
+            }
+        }
+
+        return max;
+    }
+}
