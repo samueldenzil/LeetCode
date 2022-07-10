e7178beed0358b9eee7282b4a7c383be8bedcb7d
samueldenzil <denzil.s.samuel@gmail.com>
2022-07-10 13:45:04 +0530

    July 9, 2022

---
+++ All_Problems/EvaluateBooleanBinaryTree.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/evaluate-boolean-binary-tree/
+package All_Problems;
+
+public class EvaluateBooleanBinaryTree {
+
+    public static boolean evaluateTree(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val == 1;
+        }
+
+        boolean left = evaluateTree(root.left);
+        boolean right = evaluateTree(root.right);
+
+        return operation(left, right, root.val);
+    }
+
+    private static boolean operation(boolean val1, boolean val2, int task) {
+        if (task == 2) {
+            return val1 || val2;
+        }
+        return val1 && val2;
+    }
+}
---
+++ Heaps/JumpGameVI.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/jump-game-vi/
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class JumpGameVI {
+
+    public static void main(String[] args) {
+        int[] nums = {1, -1, -2, 4, -7, 3};
+        int k = 2;
+
+        System.out.println(maxResult(nums, k));
+    }
+
+    public static int maxResult(int[] nums, int k) {
+        // Max Heap
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (b[0] - a[0])); // <score, index>
+
+        pq.add(new int[]{nums[0], 0});
+
+        int maxScore = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            while (!pq.isEmpty() && i - pq.peek()[1] > k) {
+                pq.poll();
+            }
+            int[] curr = pq.peek();
+            maxScore = nums[i] + curr[0];
+            pq.add(new int[]{maxScore, i});
+        }
+
+        return maxScore;
+    }
+}
