8f21a02875513e371125398c52804c3b18733436
samueldenzil <denzil.s.samuel@gmail.com>
2022-07-18 08:56:25 +0530

    July 17, 2022

---
+++ Strings/MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+package Strings;
+
+public class MaximumNumberOfWordsFoundInSentences {
+
+    public static void main(String[] args) {
+        String[] sentences = {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+        System.out.println(mostWordsFound(sentences));
+    }
+
+    public static int mostWordsFound(String[] sentences) {
+        int max = 0;
+
+        for (String sentence : sentences) {
+            int currWordCount = wordCount(sentence);
+            max = Math.max(max, currWordCount);
+        }
+
+        return max;
+    }
+
+    private static int wordCount(String sentence) {
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            if (c == ' ') {
+                count++;
+            }
+        }
+
+        return count + 1;
+    }
+}
