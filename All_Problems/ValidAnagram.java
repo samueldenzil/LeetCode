da4cccb9e9a1dbedafc01c5cb69abde760019149
samueldenzil <denzil.s.samuel@gmail.com>
2022-07-19 21:02:19 +0530

    July 18, 2022

--- All_Problems/ValidAnagram.java
+++ All_Problems/ValidAnagram.java
@@ -12,6 +12,30 @@ public class ValidAnagram {
     }
 
     public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] letters = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            letters[c1 - 'a']++;
+            letters[c2 - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (letters[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /* public static boolean isAnagram(String s, String t) {
         s = sortString(s);
         t = sortString(t);
 
@@ -22,5 +46,5 @@ public class ValidAnagram {
         char[] arr = str.toCharArray();
         Arrays.sort(arr);
         return new String(arr);
-    }
+    } */
 }
