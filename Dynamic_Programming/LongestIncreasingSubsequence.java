acbc4da6e320cf8b528ddb15ea5b8928128259ad
samueldenzil <denzil.s.samuel@gmail.com>
2022-08-09 09:58:39 +0530

    August 8, 2022

--- Dynamic_Programming/LongestIncreasingSubsequence.java
+++ Dynamic_Programming/LongestIncreasingSubsequence.java
@@ -13,20 +13,30 @@ public class LongestIncreasingSubsequence {
     public static int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-
-        int max = 1;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            max = Math.max(max, dp[i]);
+        int[][] dp = new int[n][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
 
-        return max;
+        return helper(0, -1, nums, dp);
+    }
+
+    private static int helper(int ind, int prev, int[] nums, int[][] dp) {
+        // base case
+        if (ind == nums.length) {
+            return 0;
+        }
+
+        if (dp[ind][prev + 1] != -1) {
+            return dp[ind][prev + 1];
+        }
+
+        int notTake = helper(ind + 1, prev, nums, dp);
+        int take = 0;
+        if (prev == -1 || nums[ind] > nums[prev]) {
+            take = 1 + helper(ind + 1, ind, nums, dp);
+        }
+
+        return dp[ind][prev + 1] = Math.max(take, notTake);
     }
 }
