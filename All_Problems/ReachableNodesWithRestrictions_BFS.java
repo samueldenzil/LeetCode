5eca46194d1eeb9c5b4f9e8e58e5dbd95ef66fc0
samueldenzil <denzil.s.samuel@gmail.com>
2022-08-10 13:47:03 +0530

    August 9, 2022

---
+++ All_Problems/ReachableNodesWithRestrictions_BFS.java
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/reachable-nodes-with-restrictions/
+package All_Problems;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class ReachableNodesWithRestrictions_BFS {
+
+    public static void main(String[] args) {
+        int n = 7;
+        int[][] edges = {{0, 1}, {1, 2}, {3, 1}, {4, 0}, {0, 5}, {5, 6}};
+        int[] restricted = {4, 5};
+        System.out.println(reachableNodes(n, edges, restricted));
+    }
+
+    public static int reachableNodes(int n, int[][] edges, int[] restricted) {
+        List<List<Integer>> adj = getList(edges, n);
+        Queue<Integer> queue = new LinkedList<>();
+        int[] visited = new int[n];
+
+        for (int it : restricted) {
+            visited[it] = -1;
+        }
+
+        visited[0] = 1;
+        queue.add(0);
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            visited[node] = 1;
+            count++;
+
+            for (int it : adj.get(node)) {
+                if (visited[it] == 0) {
+                    queue.add(it);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static List<List<Integer>> getList(int[][] edges, int n) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] row : edges) {
+            list.get(row[0]).add(row[1]);
+            list.get(row[1]).add(row[0]);
+        }
+        return list;
+    }
+}
---
+++ Graph/ReachableNodesWithRestrictions_DFS.java
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/reachable-nodes-with-restrictions/
+package Graph;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ReachableNodesWithRestrictions_DFS {
+
+    public static void main(String[] args) {
+        int n = 7;
+        int[][] edges = {{0, 1}, {1, 2}, {3, 1}, {4, 0}, {0, 5}, {5, 6}};
+        int[] restricted = {4, 5};
+        System.out.println(reachableNodes(n, edges, restricted));
+    }
+
+    public static int reachableNodes(int n, int[][] edges, int[] restricted) {
+        List<List<Integer>> adj = getList(edges, n);
+        int[] visited = new int[n];
+
+        for (int it : restricted) {
+            visited[it] = -1;
+        }
+
+        visited[0] = 1;
+        int[] count = new int[1];
+
+        dfs(0, adj, visited, count);
+        return count[0];
+    }
+
+    private static void dfs(int node, List<List<Integer>> adj, int[] visited, int[] count) {
+        visited[node] = 1;
+        count[0]++;
+        for (int it : adj.get(node)) {
+            if (visited[it] == 0) {
+                dfs(it, adj, visited, count);
+            }
+        }
+    }
+
+    private static List<List<Integer>> getList(int[][] edges, int n) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] row : edges) {
+            list.get(row[0]).add(row[1]);
+            list.get(row[1]).add(row[0]);
+        }
+        return list;
+    }
+}
