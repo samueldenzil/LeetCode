f556ba6460bfc794d729bf6758fb5415f83c5ec9
samueldenzil <denzil.s.samuel@gmail.com>
2022-08-20 10:12:08 +0530

    August 19, 2022

---
+++ All_Problems/FindWhetherPathExist.java
@@ -0,0 +1,84 @@
+// https://practice.geeksforgeeks.org/problems/find-whether-path-exist5238/1
+package All_Problems;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class FindWhetherPathExist {
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {3, 0, 3, 0, 0},
+                {3, 0, 0, 0, 3},
+                {3, 3, 3, 3, 3},
+                {0, 2, 3, 0, 0},
+                {3, 0, 0, 1, 3}
+        };
+        System.out.println(is_Possible(grid));
+    }
+
+    static class Pair {
+        int row;
+        int col;
+
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    //Function to find whether a path exists from the source to destination.
+    public static boolean is_Possible(int[][] grid) {
+        // Code here
+        int n = grid.length;
+        boolean[][] visited = new boolean[n][n];
+        int row = 0;
+        int col = 0;
+        int[] end = new int[2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    row = i;
+                    col = j;
+                } else if (grid[i][j] == 2) {
+                    end[0] = i;
+                    end[1] = j;
+                }
+            }
+        }
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(row, col));
+        visited[row][col] = true;
+
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        while (!queue.isEmpty()) {
+            row = queue.peek().row;
+            col = queue.peek().col;
+            queue.poll();
+
+            if (row == end[0] && col == end[1]) {
+                return true;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + dx[i];
+                int ncol = col + dy[i];
+
+                if (isValid(nrow, ncol, grid, visited, n)) {
+                    queue.add(new Pair(nrow, ncol));
+                    visited[nrow][ncol] = true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isValid(int row, int col, int[][] grid, boolean[][] visited, int n) {
+        return row >= 0 && row < n && col >= 0 && col < n && (grid[row][col] == 2 || grid[row][col] == 3) && !visited[row][col];
+    }
+}
---
+++ Graph/ShortestSourceToDestinationPath.java
@@ -0,0 +1,74 @@
+// https://practice.geeksforgeeks.org/problems/shortest-source-to-destination-path3544/1
+package Graph;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class ShortestSourceToDestinationPath {
+
+    static class Pair {
+        int row;
+        int col;
+        int dist;
+
+        Pair(int row, int col, int dist) {
+            this.row = row;
+            this.col = col;
+            this.dist = dist;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] A = {
+                {1, 0, 0, 0},
+                {1, 1, 0, 1},
+                {0, 1, 1, 1}
+        };
+        int X = 2;
+        int Y = 3;
+        System.out.println(shortestDistance(A.length, A[0].length, A, X, Y));
+    }
+
+    public static int shortestDistance(int N, int M, int A[][], int X, int Y) {
+        // code here
+        if (A[0][0] == 0 || A[X][Y] == 0) {
+            return -1;
+        }
+
+        boolean[][] visited = new boolean[N][M];
+        Queue<Pair> queue = new LinkedList<>();
+
+        queue.add(new Pair(0, 0, 0));
+        visited[0][0] = true;
+
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        while (!queue.isEmpty()) {
+            int row = queue.peek().row;
+            int col = queue.peek().col;
+            int dist = queue.peek().dist;
+            queue.poll();
+
+            if (row == X && col == Y) {
+                return dist;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + dx[i];
+                int ncol = col + dy[i];
+
+                if (isValid(nrow, ncol, A, visited, N, M)) {
+                    queue.add(new Pair(nrow, ncol, dist + 1));
+                    visited[nrow][ncol] = true;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private static boolean isValid(int row, int col, int[][] A, boolean[][] visited, int N, int M) {
+        return row >= 0 && row < N && col >= 0 && col < M && A[row][col] == 1 && !visited[row][col];
+    }
+}
