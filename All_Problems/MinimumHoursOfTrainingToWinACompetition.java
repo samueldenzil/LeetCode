bafd61ae2dd1b49627449faa482676d188d28a18
samueldenzil <denzil.s.samuel@gmail.com>
2022-08-22 21:23:11 +0530

    August 21, 2022

---
+++ All_Problems/MinimumHoursOfTrainingToWinACompetition.java
@@ -0,0 +1,69 @@
+// https://leetcode.com/problems/minimum-hours-of-training-to-win-a-competition/
+package All_Problems;
+
+public class MinimumHoursOfTrainingToWinACompetition {
+
+    public static void main(String[] args) {
+        int initialEnergy = 5, initialExperience = 3;
+        int[] energy = {1, 4, 3, 2};
+        int[] experience = {2, 6, 3, 1};
+        System.out.println(minNumberOfHours(initialEnergy, initialExperience, energy, experience));
+    }
+
+    public static int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
+        int ans = 0;
+
+        int start = 0;
+        int end = 10000;
+        int en = 0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (minEnergy(initialEnergy + mid, energy)) {
+                en = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        ans += en;
+
+        start = 0;
+        end = 10000;
+        int exp = 0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (minExperience(initialExperience + mid, experience)) {
+                exp = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        ans += exp;
+
+        return ans;
+    }
+
+    private static boolean minEnergy(int en, int[] energy) {
+        int n = energy.length;
+        for (int i = 0; i < n; i++) {
+            if (en <= energy[i]) {
+                return false;
+            }
+            en -= energy[i];
+        }
+        return true;
+    }
+
+    private static boolean minExperience(int exp, int[] experience) {
+        int n = experience.length;
+        for (int i = 0; i < n; i++) {
+            if (exp <= experience[i]) {
+                return false;
+            }
+            exp += experience[i];
+        }
+        return true;
+    }
+}
---
+++ Graph/AmountOfTimeForBinaryTreeToBeInfected.java
@@ -0,0 +1,118 @@
+// https://leetcode.com/problems/amount-of-time-for-binary-tree-to-be-infected/
+package Graph;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class AmountOfTimeForBinaryTreeToBeInfected {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Pair {
+        int node;
+        int t;
+
+        Pair(int node, int t) {
+            this.node = node;
+            this.t = t;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(6);
+        root.left.right.left = new TreeNode(9);
+        root.left.right.right = new TreeNode(2);
+
+        int start = 3;
+        System.out.println(amountOfTime(root, start));
+    }
+
+    public static int amountOfTime(TreeNode root, int start) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        generateAdjList(root, adj);
+
+        Queue<Pair> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        queue.add(new Pair(start, 0));
+        visited.add(start);
+
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.peek().node;
+            int t = queue.peek().t;
+            queue.poll();
+            time = t;
+            if (!adj.containsKey(node)) {
+                break;
+            }
+            for (int it : adj.get(node)) {
+                if (!visited.contains(it)) {
+                    queue.add(new Pair(it, t + 1));
+                    visited.add(it);
+                }
+            }
+        }
+
+        return time;
+    }
+
+    private static void generateAdjList(TreeNode root, Map<Integer, List<Integer>> adj) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> list = null;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    list = adj.getOrDefault(node.val, new ArrayList<>());
+                    list.add(node.left.val);
+                    adj.put(node.val, list);
+
+                    list = adj.getOrDefault(node.left.val, new ArrayList<>());
+                    list.add(node.val);
+                    adj.put(node.left.val, list);
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    list = adj.getOrDefault(node.val, new ArrayList<>());
+                    list.add(node.right.val);
+                    adj.put(node.val, list);
+
+                    list = adj.getOrDefault(node.right.val, new ArrayList<>());
+                    list.add(node.val);
+                    adj.put(node.right.val, list);
+                    queue.add(node.right);
+                }
+            }
+        }
+    }
+}
