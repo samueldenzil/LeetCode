d86f48930fef9b113c758fc0d3c70a4928c88c5c
samueldenzil <denzil.s.samuel@gmail.com>
2022-09-23 08:57:03 +0530

    September 22, 2022

---
+++ All_Problems/ReverseWordsInAStringIII.java
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/
+package All_Problems;
+
+public class ReverseWordsInAStringIII {
+
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+        int n = s.length();
+        char[] chArr = s.toCharArray();
+        int start = 0;
+        int end = 0;
+
+        while (end < n) {
+            while (end < n && chArr[end] != ' ') {
+                end++;
+            }
+            reverse(start, end - 1, chArr);
+            end++;
+            start = end;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : chArr) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
+    private static void reverse(int start, int end, char[] chArr) {
+        while (start < end) {
+            char temp = chArr[start];
+            chArr[start] = chArr[end];
+            chArr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
