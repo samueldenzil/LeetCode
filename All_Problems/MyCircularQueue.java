f54dc933b7f58082c11b5c162830094433bc9450
samueldenzil <denzil.s.samuel@gmail.com>
2022-09-26 22:33:06 +0530

    September 25, 2022

---
+++ All_Problems/MyCircularQueue.java
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/design-circular-queue/
+package All_Problems;
+
+class MyCircularQueue {
+
+    int[] queue;
+    int front, rear, length;
+
+    public MyCircularQueue(int k) {
+        queue = new int[k];
+        front = 0;
+        rear = -1;
+        length = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        rear = (rear + 1) % queue.length;
+        queue[rear] = value;
+        length++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        queue[front] = 0;
+        front = (front + 1) % queue.length;
+        length--;
+        return true;
+    }
+
+    public int Front() {
+        return !isEmpty() ? queue[front] : -1;
+    }
+
+    public int Rear() {
+        return !isEmpty() ? queue[rear] : -1;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public boolean isFull() {
+        return length == queue.length;
+    }
+
+    public static void main(String[] args) {
+        MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+        System.out.println(myCircularQueue.enQueue(1));
+        System.out.println(myCircularQueue.enQueue(2));
+        System.out.println(myCircularQueue.enQueue(3));
+        System.out.println(myCircularQueue.enQueue(4));
+        System.out.println(myCircularQueue.Rear());
+        System.out.println(myCircularQueue.isFull());
+        System.out.println(myCircularQueue.deQueue());
+        System.out.println(myCircularQueue.enQueue(4));
+        System.out.println(myCircularQueue.Rear());
+    }
+}
---
+++ Arrays/LongestSubarrayWithMaximumBitwiseAND.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/
+package Arrays;
+
+public class LongestSubarrayWithMaximumBitwiseAND {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 3, 2, 2};
+        System.out.println(longestSubarray(nums));
+    }
+
+    public static int longestSubarray(int[] nums) {
+        int maxElement = 0;
+        for (int it : nums) {   // finding out the max element
+            maxElement = Math.max(maxElement, it);
+        }
+
+        int count = 0;
+        int ans = 1;
+
+        for (int i = 0; i < nums.length; i++) { // finding continuous occurrence of the max element
+            if (nums[i] == maxElement) {
+                count++;
+            } else {
+                count = 0;
+            }
+            ans = Math.max(ans, count);
+        }
+
+        return ans;
+    }
+}
