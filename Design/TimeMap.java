cc7d3626917b7fcee33c34df7d5a160f587a41d1
samueldenzil <denzil.s.samuel@gmail.com>
2022-10-07 10:06:47 +0530

    October 6, 2022

---
+++ Design/TimeMap.java
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/time-based-key-value-store/
+package Design;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class TimeMap {
+    Map<String, TreeMap<Integer, String>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) {
+            map.put(key, new TreeMap<>());
+        }
+        map.get(key).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        TreeMap<Integer, String> treeMap = map.get(key);
+        if (treeMap == null) {
+            return "";
+        }
+        Integer floor = treeMap.floorKey(timestamp);
+        if (floor == null) {
+            return "";
+        }
+        return treeMap.get(floor);
+    }
+
+    public static void main(String[] args) {
+        TimeMap timeMap = new TimeMap();
+        timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+        System.out.println(timeMap.get("foo", 1));         // return "bar"
+        System.out.println(timeMap.get("foo", 3));         // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+        timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4.
+        System.out.println(timeMap.get("foo", 4));         // return "bar2"
+        System.out.println(timeMap.get("foo", 5));         // return "bar2"
+    }
+}
