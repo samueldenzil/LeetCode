18600a7105a177489df39fe2bac37e79f76ea6de
samueldenzil <denzil.s.samuel@gmail.com>
2022-10-08 17:22:19 +0530

    October 7, 2022

---
+++ All_Problems/RemoveLetterToEqualizeFrequency.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/remove-letter-to-equalize-frequency/
+package All_Problems;
+
+public class RemoveLetterToEqualizeFrequency {
+
+    public static void main(String[] args) {
+        String word = "abcc";
+        System.out.println(equalFrequency(word));
+    }
+
+    public static boolean equalFrequency(String word) {
+        int n = word.length();
+        int[] letters = new int[26];
+        for (int i = 0; i < n; i++) {
+            char c = word.charAt(i);
+            letters[c - 'a']++;
+        }
+
+        // brute force
+        for (int i = 0; i < n; i++) {
+            char c = word.charAt(i);
+            letters[c - 'a']--;
+            if (isEqual(letters)) {
+                return true;
+            }
+            letters[c - 'a']++;
+        }
+
+        return false;
+    }
+
+    private static boolean isEqual(int[] letters) {
+        int c = 0;
+        for (int it : letters) {
+            if (it == 0) {
+                continue;
+            } else if (c == 0) {
+                c = it;
+            } else if (c == it) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ SlidingWindow/SubstringsOfLengthKWithKMinus1DistinctElements.java
@@ -0,0 +1,49 @@
+// https://practice.geeksforgeeks.org/problems/substrings-of-length-k-with-k-1-distinct-elements/1
+package SlidingWindow;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class SubstringsOfLengthKWithKMinus1DistinctElements {
+
+    public static void main(String[] args) {
+        String S = "aabab";
+        int K = 3;
+        System.out.println(countOfSubstrings(S, K));
+    }
+
+    public static int countOfSubstrings(String S, int K) {
+        // code here
+        Map<Character, Integer> map = new HashMap<>();
+        int ans = 0;
+
+        for (int i = 0; i < K; i++) {
+            char c = S.charAt(i);
+            if (!map.containsKey(c)) {
+                map.put(c, 0);
+            }
+            map.put(c, map.get(c) + 1);
+        }
+        if (map.size() == K - 1) {
+            ans++;
+        }
+
+        for (int i = K; i < S.length(); i++) {
+            char c1 = S.charAt(i - K);
+            map.put(c1, map.get(c1) - 1);
+            if (map.get(c1) == 0) {
+                map.remove(c1);
+            }
+            char c2 = S.charAt(i);
+            if (!map.containsKey(c2)) {
+                map.put(c2, 0);
+            }
+            map.put(c2, map.get(c2) + 1);
+            if (map.size() == K - 1) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
