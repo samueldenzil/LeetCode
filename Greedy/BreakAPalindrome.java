5887d94bba0afdd23e48ec5b31f7fa07e1c993f7
samueldenzil <denzil.s.samuel@gmail.com>
2022-10-11 18:23:01 +0530

    October 10, 2022

---
+++ Greedy/BreakAPalindrome.java
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/break-a-palindrome/
+package Greedy;
+
+public class BreakAPalindrome {
+
+    public static void main(String[] args) {
+        String palindrome = "abccba";
+        System.out.println(breakPalindrome(palindrome));
+    }
+
+    public static String breakPalindrome(String palindrome) {
+        if (palindrome.length() == 1) {
+            return "";
+        }
+
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i < palindrome.length()) {
+            char c = palindrome.charAt(i);
+            int index = c - 'a';
+            if (index == 0) {
+                sb.append(c);
+                i++;
+                continue;
+            }
+            sb.append('a');
+            i++;
+            break;
+        }
+
+        sb.append(palindrome.substring(i));
+
+        if (isPalindrome(sb)) {
+            return palindrome.substring(0, palindrome.length() - 1) + "b";
+        }
+        return sb.toString();
+    }
+
+    private static boolean isPalindrome(StringBuilder sb) {
+        int i = 0;
+        int j = sb.length() - 1;
+        while (i < j) {
+            if (sb.charAt(i) != sb.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
