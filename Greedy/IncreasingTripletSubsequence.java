f6f51bf738aeb7966cbb1dd46f1ceee8aa507c61
samueldenzil <denzil.s.samuel@gmail.com>
2022-10-12 23:35:31 +0700

    October 11, 2022

---
+++ Greedy/IncreasingTripletSubsequence.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+package Greedy;
+
+public class IncreasingTripletSubsequence {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 1, 5, 0, 4, 6};
+        System.out.println(increasingTriplet(nums));
+    }
+
+    public static boolean increasingTriplet(int[] nums) {
+        int left = Integer.MAX_VALUE;
+        int mid = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > mid) {
+                return true;
+            } else if (nums[i] > left && nums[i] < mid) {
+                mid = nums[i];
+            } else if (nums[i] < left) {
+                left = nums[i];
+            }
+        }
+
+        return false;
+    }
+}
