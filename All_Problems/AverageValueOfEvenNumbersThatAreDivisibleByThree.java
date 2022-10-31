9b9c86207df0c70d5d1a1bfb6226ceff49f4575d
samueldenzil <denzil.s.samuel@gmail.com>
2022-10-31 21:47:23 +0530

    October 30, 2022

---
+++ All_Problems/AverageValueOfEvenNumbersThatAreDivisibleByThree.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/average-value-of-even-numbers-that-are-divisible-by-three
+package All_Problems;
+
+public class AverageValueOfEvenNumbersThatAreDivisibleByThree {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 4, 9, 10};
+        System.out.println(averageValue(nums));
+    }
+
+    public static int averageValue(int[] nums) {
+        int avg = 0;
+        int n = 0;
+
+        for (int it : nums) {
+            if (it % 2 == 0 && it % 3 == 0) {
+                avg += it;
+                n++;
+            }
+        }
+
+        return n != 0 ? avg / n : 0;
+    }
+}
---
+++ Graph/DijkstraAlgorithm.java
@@ -0,0 +1,47 @@
+// https://practice.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/0
+package Graph;
+
+import java.util.*;
+
+public class DijkstraAlgorithm {
+    
+    static class Pair {
+        int node;
+        int dist;
+
+        Pair(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+    }
+
+    //Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    public static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
+        // Write your code here
+        // Min Heap
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
+        int[] distArr = new int[V];
+        Arrays.fill(distArr, (int) 1e9);
+        distArr[S] = 0;
+        pq.add(new Pair(S, 0));
+
+        while (!pq.isEmpty()) {
+            Pair p = pq.poll();
+            int node = p.node;
+            int dist = p.dist;
+
+            for (int i = 0; i < adj.get(node).size(); i++) {
+                int adjNode = adj.get(node).get(i).get(0);
+                int edgeWeight = adj.get(node).get(i).get(1);
+
+                if (dist + edgeWeight < distArr[adjNode]) {
+                    distArr[adjNode] = dist + edgeWeight;
+                    pq.add(new Pair(adjNode, distArr[adjNode]));
+                }
+            }
+        }
+
+        return distArr;
+    }
+}
