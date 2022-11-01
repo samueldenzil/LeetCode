dd2abc8169605cf51e0d2618ba86a178d22851e2
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-01 19:46:33 +0530

    October 31, 2022

---
+++ Arrays/ToeplitzMatrix.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/toeplitz-matrix/
+package Arrays;
+
+public class ToeplitzMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 1, 2, 3},
+                {9, 5, 1, 2}
+        };
+        System.out.println(isToeplitzMatrix(matrix));
+    }
+
+    public static boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i - 1][j - 1] != matrix[i][j]) { // check whether each value is equal to the value of it's top-left neighbor.
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
