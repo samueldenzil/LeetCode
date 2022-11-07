a33adae8f8b7590185e1d9a17b16835c04f6fa0c
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-07 19:56:07 +0530

    November 6, 2022

---
+++ Arrays/Maximum69Number.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/maximum-69-number/
+package Arrays;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Maximum69Number {
+
+    public static void main(String[] args) {
+        int num = 9669;
+        System.out.println(maximum69Number(num));
+    }
+
+    public static int maximum69Number (int num) {
+        List<Integer> list = new ArrayList<>();
+        while (num > 0) {
+            int remainder = num % 10;
+            list.add(remainder);
+            num /= 10;
+        }
+        boolean flag = true;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i) == 6 && flag) {
+                list.set(i, 9);
+                flag = false;
+            }
+            num *= 10;
+            num += list.get(i);
+        }
+        return num;
+    }
+}
