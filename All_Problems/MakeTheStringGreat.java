8de3723ebf1b0ad11d62179259f51f47ce77cd5b
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-09 20:02:50 +0530

    November 7, 2022

---
+++ All_Problems/MakeTheStringGreat.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/make-the-string-great/
+package All_Problems;
+
+import java.util.Stack;
+
+public class MakeTheStringGreat {
+
+    public static void main(String[] args) {
+        String s = "leEeetcode";
+        System.out.println(makeGood(s));
+    }
+
+    public static String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && Math.abs(c - stack.peek()) == 32) {
+                stack.pop();
+            } else {
+                stack.add(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
