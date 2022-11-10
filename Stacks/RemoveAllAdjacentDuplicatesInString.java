a734f5c6cc0a1dbec93dcc7d98f06b3cf1e39f06
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-10 09:07:16 +0530

    November 8, 2022

---
+++ Stacks/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+package Stacks;
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInString {
+
+    public static void main(String[] args) {
+        String s = "abbaca";
+        System.out.println(removeDuplicates(s));
+    }
+
+    public static String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && c == stack.peek()) {
+                stack.pop();
+            } else {
+                stack.add(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
