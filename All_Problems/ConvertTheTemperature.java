ab3994f0ed9352d9c34db33a5517962f051dc3ff
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-13 11:07:07 +0530

    November 12, 2022

---
+++ All_Problems/ConvertTheTemperature.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/convert-the-temperature/
+package All_Problems;
+
+import java.util.Arrays;
+
+public class ConvertTheTemperature {
+
+    public static void main(String[] args) {
+        double celsius = 36.50;
+        System.out.println(Arrays.toString(convertTemperature(celsius)));
+    }
+
+    public static double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return new double[]{kelvin, fahrenheit};
+    }
+}
