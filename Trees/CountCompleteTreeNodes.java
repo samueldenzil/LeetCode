7d1e96d2334e2e4736ed390f54ac86d8666515ba
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-16 16:33:04 +0530

    November 15, 2022

---
+++ Trees/CountCompleteTreeNodes.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/count-complete-tree-nodes/
+package Trees;
+
+public class CountCompleteTreeNodes {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return 1 + left + right;
+    }
+}
