7a5bc691a7a2790fdaccdb325d40741b914d974f
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-18 20:08:00 +0530

    November 17, 2022

---
+++ All_Problems/UglyNumber.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/ugly-number/
+package All_Problems;
+
+public class UglyNumber {
+
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(isUgly(n));
+    }
+
+    public static boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        for (int it : new int[]{2, 3, 5}) {
+            while (n % it == 0) {
+                n /= it;
+            }
+        }
+        return n == 1;
+    }
+}
