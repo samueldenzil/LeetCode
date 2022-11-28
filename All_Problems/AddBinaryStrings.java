78275a61fb9f5ba71cb38057853be6f9a3faf6f8
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-28 08:01:45 +0530

    November 27, 2022

---
+++ All_Problems/AddBinaryStrings.java
@@ -0,0 +1,41 @@
+// https://practice.geeksforgeeks.org/problems/add-binary-strings3805/1
+package All_Problems;
+
+public class AddBinaryStrings {
+
+    public static void main(String[] args) {
+        String A = "10", B = "1000000";
+        System.out.println(addBinary(A, B));
+    }
+
+    public static String addBinary(String A, String B) {
+        // code here
+        int i = A.length() - 1;
+        int j = B.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0 || j >= 0) {
+            int digit1 = 0;
+            if (i >= 0) {
+                digit1 = A.charAt(i) - '0';
+                i--;
+            }
+            int digit2 = 0;
+            if (j >= 0) {
+                digit2 = B.charAt(j) - '0';
+                j--;
+            }
+
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 2;
+            sb.append(sum % 2);
+        }
+        sb.append(carry);
+        i = sb.length() - 1;
+        while (i >= 0 && sb.charAt(i) == '0') {
+            i--;
+        }
+        return sb.reverse().substring(sb.length() - i - 1);
+    }
+}
---
+++ All_Problems/FindThePivotInteger.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/find-the-pivot-integer/
+package All_Problems;
+
+public class FindThePivotInteger {
+
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println(pivotInteger(n));
+    }
+
+    public static int pivotInteger(int n) {
+        for (int i = 1; i <= n; i++) {
+            int s1 = getSumOfNNums(i);
+            int s2 = getSumOfNNums(n) - s1 + i;
+            if (s1 == s2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static int getSumOfNNums(int n) {
+        return n * (n + 1) / 2;
+    }
+}
