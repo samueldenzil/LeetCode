ffb77578f85ef57d41684f293f2ffadcab1e807e
samueldenzil <denzil.s.samuel@gmail.com>
2022-11-30 11:48:11 +0530

    November 29, 2022

---
+++ All_Problems/RandomizedSet.java
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/insert-delete-getrandom-o1
+package All_Problems;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class RandomizedSet {
+
+    List<Integer> list;
+    Map<Integer, Integer> map;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        int lastElement = list.get(list.size() - 1);
+
+        if (lastElement == val) {
+            list.remove(list.size() - 1);
+            map.remove(val);
+            return true;
+        }
+
+        int index = map.get(val);
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        int randomIndex = (int) Math.floor(Math.random() * list.size());
+        return list.get(randomIndex);
+    }
+
+    public static void main(String[] args) {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(1)); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+        System.out.println(randomizedSet.remove(2)); // Returns false as 2 does not exist in the set.
+        System.out.println(randomizedSet.insert(2)); // Inserts 2 to the set, returns true. Set now contains [1,2].
+        System.out.println(randomizedSet.getRandom()); // getRandom() should return either 1 or 2 randomly.
+        System.out.println(randomizedSet.remove(1)); // Removes 1 from the set, returns true. Set now contains [2].
+        System.out.println(randomizedSet.insert(2)); // 2 was already in the set, so return false.
+        System.out.println(randomizedSet.getRandom()); // Since 2 is the only number in the set, getRandom() will always return 2.
+    }
+}
---
+++ All_Problems/RemoveNodesFromLinkedList.java
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/remove-nodes-from-linked-list/
+package All_Problems;
+
+public class RemoveNodesFromLinkedList {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 13, 3, 8};
+        ListNode head = generateLinkedList(arr);
+
+        ListNode ans = removeNodes(head);
+        while (ans != null) {
+            System.out.print(ans.val + " ");
+            ans = ans.next;
+        }
+    }
+
+    public static ListNode removeNodes(ListNode head) {
+        ListNode rev = reverse(head);
+        ListNode temp = rev;
+        int max = rev.val;
+        while (temp != null) {
+            while (temp.next != null && temp.next.val < max) {
+                temp.next = temp.next.next;
+            }
+            temp = temp.next;
+            if (temp != null) {
+                max = Math.max(max, temp.val);
+            }
+        }
+
+        return reverse(rev);
+    }
+
+    private static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    private static ListNode generateLinkedList(int[] arr) {
+        ListNode head = new ListNode(arr[0]);
+        ListNode temp = head;
+        for (int i = 1; i < arr.length; i++) {
+            temp.next = new ListNode(arr[i]);
+            temp = temp.next;
+        }
+        return head;
+    }
+}
