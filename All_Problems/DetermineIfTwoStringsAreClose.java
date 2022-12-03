f61900208042718b87c6a1628e78ffb273e66477
samueldenzil <denzil.s.samuel@gmail.com>
2022-12-03 18:22:36 +0530

    December 2, 2022

---
+++ All_Problems/DetermineIfTwoStringsAreClose.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/determine-if-two-strings-are-close/
+package All_Problems;
+
+import java.util.Arrays;
+
+public class DetermineIfTwoStringsAreClose {
+
+    public static void main(String[] args) {
+        String word1 = "cabbba", word2 = "abbccc";
+        System.out.println(closeStrings(word1, word2));
+    }
+
+    public static boolean closeStrings(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        if (n != m) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            freq1[word1.charAt(i) - 'a']++;
+            freq2[word2.charAt(i) - 'a']++;
+        }
+
+        // point 1
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != 0 && freq2[i] != 0) {
+                continue;
+            } else if (freq1[i] == 0 && freq2[i] == 0) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+
+        // point 2
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
