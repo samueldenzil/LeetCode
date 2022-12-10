742bbc59c86cf0bfc3429817b0d43ab8f32cff5d
samueldenzil <denzil.s.samuel@gmail.com>
2022-12-10 23:36:04 +0530

    December 9, 2022

---
+++ Arrays/MaximumValueOfAStringInAnArray.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/maximum-value-of-a-string-in-an-array/
+package Arrays;
+
+public class MaximumValueOfAStringInAnArray {
+
+    public static void main(String[] args) {
+        String[] strs = {"alic3", "bob", "3", "4", "00000"};
+        System.out.println(maximumValue(strs));
+    }
+
+    public static int maximumValue(String[] strs) {
+        int max = Integer.MIN_VALUE;
+        for (String word : strs) {
+            int count = getCount(word);
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+
+    private static int getCount(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c - '0' >= 0 && c - '9' <= 9) {
+                continue;
+            } else {
+                return s.length();
+            }
+        }
+
+        return Integer.parseInt(s);
+    }
+}
