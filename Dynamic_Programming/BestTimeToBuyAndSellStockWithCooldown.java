31948384f18a4721320ba8b5ed7444509067f335
samueldenzil <denzil.s.samuel@gmail.com>
2022-12-24 09:56:39 +0530

    December 23, 2022

--- Dynamic_Programming/BestTimeToBuyAndSellStockWithCooldown.java
+++ Dynamic_Programming/BestTimeToBuyAndSellStockWithCooldown.java
@@ -12,37 +12,94 @@ public class BestTimeToBuyAndSellStockWithCooldown {
 
     public static int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
+        int[][] dp = new int[n + 2][2];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
         return helper(0, true, prices, dp);
     }
 
+    // Memoization
     // TC: O(n * 2)
     // SC: O(n * 2) + O(n)
-    private static int helper(int i, boolean buy, int[] prices, int[][] dp) {
-        if (i >= prices.length) {
+    private static int helper(int day, boolean buy, int[] prices, int[][] dp) {
+        // base case
+        if (day >= prices.length) {
             return 0;
         }
         int j = buy ? 1 : 0;
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if (dp[day][j] != -1) {
+            return dp[day][j];
         }
 
-        // no transaction today
-        int noTransaction = helper(i + 1, buy, prices, dp);
-
-        int transaction = 0;
-        if (buy) {
-            // buying stocks
-            transaction = -prices[i] + helper(i + 1, false, prices, dp);
-        } else {
-            // selling stocks
-            transaction = prices[i] + helper(i + 2, true, prices, dp);
+        int profit = 0;
+        if (buy) {  // buy
+            int take = helper(day + 1, false, prices, dp) - prices[day];
+            int notTake = helper(day + 1, true, prices, dp);
+            profit = Math.max(take, notTake);
+        } else {    // sell
+            int sell = prices[day] + helper(day + 2, true, prices, dp);
+            int notSell = helper(day + 1, false, prices, dp);
+            profit = Math.max(sell, notSell);
         }
-
-        return dp[i][j] = Math.max(noTransaction, transaction);
+        return dp[day][j] = profit;
     }
+
+
+    // Tabulation
+    // TC: O(n * 2)
+    // SC: O(n * 2)
+    /* public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n + 2][2];
+
+        for (int day = n - 1; day >= 0; day--) {
+            for (int j = 0; j <= 1; j++) {
+                int profit = 0;
+                if (j == 1) {  // buy
+                    int take = dp[day + 1][0] - prices[day];
+                    int notTake = dp[day + 1][1];
+                    profit = Math.max(take, notTake);
+                } else {    // sell
+                    int sell = prices[day] + dp[day + 2][1];
+                    int notSell = dp[day + 1][0];
+                    profit = Math.max(sell, notSell);
+                }
+                dp[day][j] = profit;
+            }
+        }
+
+        return dp[0][1];
+    } */
+
+
+    // Tabulation + Space Optimization
+    // TC: O(n * 2)
+    // SC: O(2 * 3) ~ O(1)
+    /* public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] front1 = new int[2];
+        int[] front2 = new int[2];
+
+        for (int day = n - 1; day >= 0; day--) {
+            int[] curr = new int[2];
+            for (int j = 0; j <= 1; j++) {
+                int profit = 0;
+                if (j == 1) {  // buy
+                    int take = front1[0] - prices[day];
+                    int notTake = front1[1];
+                    profit = Math.max(take, notTake);
+                } else {    // sell
+                    int sell = prices[day] + front2[1];
+                    int notSell = front1[0];
+                    profit = Math.max(sell, notSell);
+                }
+                curr[j] = profit;
+            }
+            front2 = front1;
+            front1 = curr;
+        }
+
+        return front1[1];
+    } */
 }
