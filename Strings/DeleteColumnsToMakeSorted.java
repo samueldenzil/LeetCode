e38fe18c46a258c4986e79d89c4601d8457776b8
samueldenzil <denzil.s.samuel@gmail.com>
2023-01-04 09:13:55 +0530

    January 3, 2023

---
+++ Strings/DeleteColumnsToMakeSorted.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/delete-columns-to-make-sorted/
+package Strings;
+
+public class DeleteColumnsToMakeSorted {
+
+    public static void main(String[] args) {
+        String[] strs = {"cba", "daf", "ghi"};
+        System.out.println(minDeletionSize(strs));
+    }
+
+    public static int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        int removeCount = 0;
+
+        for (int j = 0; j < m; j++) {
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                char ch = strs[i].charAt(j);
+                if (ch - 'a' < max) {
+                    removeCount++;
+                    break;
+                } else {
+                    max = ch - 'a';
+                }
+            }
+        }
+
+        return removeCount;
+    }
+}
