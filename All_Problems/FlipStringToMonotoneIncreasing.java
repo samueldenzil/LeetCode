bf57d1105d2aa70d87a29434f031bedc90403829
samueldenzil <denzil.s.samuel@gmail.com>
2023-01-18 18:31:45 +0530

    January 17, 2023

--- All_Problems/FlipStringToMonotoneIncreasing.java
+++ All_Problems/FlipStringToMonotoneIncreasing.java
@@ -10,6 +10,9 @@ public class FlipStringToMonotoneIncreasing {
         System.out.println(minFlipsMonoIncr(s));
     }
 
+    // Memoization
+    // TC: O(n * 2)
+    // SC: O(n * 2) + O(n)
     public static int minFlipsMonoIncr(String s) {
         int n = s.length();
         int[][] dp = new int[n + 1][2];
@@ -51,4 +54,84 @@ public class FlipStringToMonotoneIncreasing {
 
         return dp[idx][prev] = Math.min(flip, notFlip);
     }
+
+    
+
+    // Tabulation
+    // TC: O(n * 2)
+    // SC: O(n * 2)
+    /* public static int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int[][] dp = new int[n + 1][2];
+
+        // base case
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prev = 0; prev <= 1; prev++) {
+                int flip = Integer.MAX_VALUE;
+                int notFlip = Integer.MAX_VALUE;
+                if (s.charAt(idx) == '0') {
+                    if (prev == 0) {  // 00
+                        notFlip = dp[idx + 1][0];   // 00
+                        flip = 1 + dp[idx + 1][1];  // 01
+                    } else if (prev == 1) {    // 10
+                        flip = 1 + dp[idx + 1][1];  // 11
+                    }
+                } else if (s.charAt(idx) == '1') {
+                    if (prev == 0) {  // 01
+                        notFlip = dp[idx + 1][1];   // 01
+                        flip = 1 + dp[idx + 1][0];  // 00
+                    } else if (prev == 1) {    // 11
+                        notFlip = dp[idx + 1][1];   // 11
+                    }
+                }
+                dp[idx][prev] = Math.min(flip, notFlip);
+            }
+        }
+
+        return dp[0][0];
+    } */
+
+    
+
+    // Tabulation + Space Optimization
+    // TC: O(n * 2)
+    // SC: O(2 * 2) ~ O(1)
+    /* public static int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int[] front = new int[2];
+
+        // base case
+        front[0] = 0;
+        front[1] = 0;
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            int[] curr = new int[2];
+            for (int prev = 0; prev <= 1; prev++) {
+                int flip = Integer.MAX_VALUE;
+                int notFlip = Integer.MAX_VALUE;
+                if (s.charAt(idx) == '0') {
+                    if (prev == 0) {  // 00
+                        notFlip = front[0];   // 00
+                        flip = 1 + front[1];  // 01
+                    } else if (prev == 1) {    // 10
+                        flip = 1 + front[1];  // 11
+                    }
+                } else if (s.charAt(idx) == '1') {
+                    if (prev == 0) {  // 01
+                        notFlip = front[1];   // 01
+                        flip = 1 + front[0];  // 00
+                    } else if (prev == 1) {    // 11
+                        notFlip = front[1];   // 11
+                    }
+                }
+                curr[prev] = Math.min(flip, notFlip);
+            }
+            front = curr;
+        }
+
+        return front[0];
+    } */
 }
