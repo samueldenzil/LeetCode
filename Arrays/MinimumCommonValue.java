5a1672437dd4da3301d0b2c0f00ceb214651613c
samueldenzil <denzil.s.samuel@gmail.com>
2023-01-21 22:18:33 +0530

    January 20, 2023

---
+++ Arrays/MinimumCommonValue.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/minimum-common-value/
+package Arrays;
+
+public class MinimumCommonValue {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3}, nums2 = {2, 4};
+        System.out.println(getCommon(nums1, nums2));
+    }
+
+    public static int getCommon(int[] nums1, int[] nums2) {
+        for (int it : nums1) {
+            if (binarySearch(nums2, it)) {
+                return it;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
