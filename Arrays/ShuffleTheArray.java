3fc07eb6661560f7901b0e57c6ed628a4ae66d0d
samueldenzil <denzil.s.samuel@gmail.com>
2023-02-07 21:55:13 +0530

    February 6, 2023

--- Arrays/ShuffleTheArray.java
+++ Arrays/ShuffleTheArray.java
@@ -1,7 +1,16 @@
 package Arrays;
 
+import java.util.Arrays;
+
 class ShuffleTheArray {
-    public int[] shuffle(int[] nums, int n) {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 1, 3, 4, 7};
+        int n = 3;
+        System.out.println(Arrays.toString(shuffle(nums, n)));
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
         int[] ans = new int[2 * n];
         for (int i = 0; i < n; i++) {
             ans[i * 2] = nums[i];
