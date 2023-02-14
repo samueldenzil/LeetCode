5e7980bb10611dac36f1e3605a00f3443117992a
samueldenzil <denzil.s.samuel@gmail.com>
2023-02-14 22:17:22 +0530

    February 13, 2023

---
+++ All_Problems/CountOddNumbersInAnIntervalRange.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+package All_Problems;
+
+public class CountOddNumbersInAnIntervalRange {
+
+    public static void main(String[] args) {
+        int low = 3, high = 7;
+        System.out.println(countOdds(low, high));
+    }
+
+    public static int countOdds(int low, int high) {
+        // If low is even, make it odd.
+        if ((low & 1) == 0) {
+            low++;
+        }
+
+        // low could become greater than high due to incrementation
+        // if it is, the answer is 0; otherwise, use the formula.
+        return low > high ? 0 : (high - low) / 2 + 1;
+    }
+}
