123167efa7e53347e47418c9ac74323557be5daa
samueldenzil <denzil.s.samuel@gmail.com>
2023-02-21 10:57:21 +0530

    February 20, 2023

---
+++ Searching/SearchInsertPosition.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/search-insert-position/
+package Searching;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        System.out.println(searchInsert(nums, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
