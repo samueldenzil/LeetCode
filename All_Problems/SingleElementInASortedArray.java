9433fd8462d33b3a4d0f66d8f641d385f90e4e06
samueldenzil <denzil.s.samuel@gmail.com>
2023-02-22 11:38:46 +0530

    February 21, 2023

---
+++ All_Problems/SingleElementInASortedArray.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/single-element-in-a-sorted-array/
+package All_Problems;
+
+public class SingleElementInASortedArray {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+        System.out.println(singleNonDuplicate(nums));
+    }
+
+    public static int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == nums[mid ^ 1]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return nums[start];
+    }
+    // in order to check for the left half
+    // 1st instance -> even index
+    // 2nd instance -> odd index, i need to check on the left if the 1st instance of that number is there or not
+}
