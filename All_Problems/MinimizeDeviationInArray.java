85520c78caba6c0b3dba4c819740ffa2937a8e91
samueldenzil <denzil.s.samuel@gmail.com>
2023-02-25 19:10:00 +0530

    February 24, 2023

---
+++ All_Problems/MinimizeDeviationInArray.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/minimize-deviation-in-array/
+package All_Problems;
+
+import java.util.TreeSet;
+
+public class MinimizeDeviationInArray {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(minimumDeviation(nums));
+    }
+
+    public static int minimumDeviation(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int it : nums) {
+            if (it % 2 == 1) {
+                set.add(it * 2);
+            } else {
+                set.add(it);
+            }
+        }
+
+        int diff = Integer.MAX_VALUE;
+        while (true) {
+            int maxVal = set.last();
+            int minVal = set.first();
+            int currDiff = maxVal - minVal;
+            diff = Math.min(diff, currDiff);
+
+            if (maxVal % 2 == 1) {
+                break;
+            }
+
+            set.remove(maxVal);
+            set.add(maxVal / 2);
+        }
+
+        return diff;
+    }
+}
