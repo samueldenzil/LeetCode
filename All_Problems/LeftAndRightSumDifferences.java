374583c1d8c54c4b1d5916f92142050dcfcd3af1
samueldenzil <denzil.s.samuel@gmail.com>
2023-02-27 19:34:48 +0530

    February 26, 2023

---
+++ All_Problems/LeftAndRightSumDifferences.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/left-and-right-sum-differences/
+package All_Problems;
+
+import java.util.Arrays;
+
+public class LeftAndRightSumDifferences {
+
+    public static void main(String[] args) {
+        int[] nums = {10, 4, 8, 3};
+        System.out.println(Arrays.toString(leftRigthDifference(nums)));
+    }
+
+    public static int[] leftRigthDifference(int[] nums) {
+        int n = nums.length;
+
+        int[] leftSum = new int[n];
+        for (int i = 1; i < n; i++) {
+            leftSum[i] += (leftSum[i - 1] + nums[i - 1]);
+        }
+
+        int[] rightSum = new int[n];
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] += (rightSum[i + 1] + nums[i + 1]);
+        }
+
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = Math.abs(leftSum[i] - rightSum[i]);
+        }
+
+        return answer;
+    }
+}
