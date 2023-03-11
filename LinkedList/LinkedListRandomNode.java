d0cd74c9cd3f48c6861f1b2173f7d487d79fb070
samueldenzil <denzil.s.samuel@gmail.com>
2023-03-11 09:42:20 +0530

    March 10, 2023

---
+++ LinkedList/LinkedListRandomNode.java
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/linked-list-random-node/
+package LinkedList;
+
+import java.util.Random;
+
+public class LinkedListRandomNode {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        ListNode head = generateLinkedList(arr);
+
+        LinkedListRandomNode linkedListRandomNode = new LinkedListRandomNode(head);
+        System.out.println(linkedListRandomNode.getRandom());
+        System.out.println(linkedListRandomNode.getRandom());
+        System.out.println(linkedListRandomNode.getRandom());
+        System.out.println(linkedListRandomNode.getRandom());
+        System.out.println(linkedListRandomNode.getRandom());
+    }
+
+    private ListNode head;
+    private Random rand;
+
+    public LinkedListRandomNode(ListNode head) {
+        this.head = head;
+        rand = new Random();
+    }
+
+    public int getRandom() {
+        int res = -1;
+        ListNode t = head;
+        for (int i = 1; t != null; i++) {
+            if (rand.nextInt(i) == i - 1) {
+                res = t.val;
+            }
+            t = t.next;
+        }
+        return res;
+    }
+
+    private static ListNode generateLinkedList(int[] arr) {
+        ListNode head = new ListNode(arr[0]);
+        ListNode temp = head;
+        for (int i = 1; i < arr.length; i++) {
+            temp.next = new ListNode(arr[i]);
+            temp = temp.next;
+        }
+        return head;
+    }
+}
