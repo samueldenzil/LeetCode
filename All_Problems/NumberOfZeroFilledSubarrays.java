ad851495f0e0fd268fc0bcd56b0aa0ff7a71e553
samueldenzil <denzil.s.samuel@gmail.com>
2023-03-22 08:51:40 +0530

    March 21, 2023

---
+++ All_Problems/NumberOfZeroFilledSubarrays.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/number-of-zero-filled-subarrays/
+package All_Problems;
+
+public class NumberOfZeroFilledSubarrays {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 0, 0, 2, 0, 0, 4};
+        System.out.println(zeroFilledSubarray(nums));
+    }
+
+    public static long zeroFilledSubarray(int[] nums) {
+        int n = nums.length;
+        long ans = 0;
+        long count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                ans += (count * (count + 1)) / 2;
+                count = 0;
+            }
+        }
+        if (count != 0) {
+            ans += (count * (count + 1)) / 2;
+        }
+
+        return ans;
+    }
+}
