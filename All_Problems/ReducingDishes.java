3bc8ed74cbd6232c1d54f0c345988bb59ca34c56
samueldenzil <denzil.s.samuel@gmail.com>
2023-03-29 07:20:19 +0530

    March 28, 2023

---
+++ All_Problems/ReducingDishes.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/reducing-dishes/
+package All_Problems;
+
+import java.util.Arrays;
+
+public class ReducingDishes {
+
+    public static void main(String[] args) {
+        int[] satisfaction = {-1, -8, 0, 5, -9};
+        System.out.println(maxSatisfaction(satisfaction));
+    }
+
+    public static int maxSatisfaction(int[] satisfaction) {
+        int n = satisfaction.length;
+        Arrays.sort(satisfaction);
+
+        int[][] dp = new int[n][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return helper(0, 1, satisfaction, dp);
+    }
+
+    private static int helper(int idx, int t, int[] satisfaction, int[][] dp) {
+        if (idx >= satisfaction.length) {
+            return 0;
+        }
+
+        if (dp[idx][t] != -1) {
+            return dp[idx][t];
+        }
+
+        // take
+        int take = (satisfaction[idx] * t) + helper(idx + 1, t + 1, satisfaction, dp);
+
+        // not take
+        int notTake = 0 + helper(idx + 1, t, satisfaction, dp);
+
+        return dp[idx][t] = Math.max(take, notTake);
+    }
+}
