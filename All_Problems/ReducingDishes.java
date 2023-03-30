7edfb6b2ebc688c63c2c68f03450bcc29fa63c5f
samueldenzil <denzil.s.samuel@gmail.com>
2023-03-30 07:51:31 +0530

    March 29, 2023

--- All_Problems/ReducingDishes.java
+++ All_Problems/ReducingDishes.java
@@ -10,6 +10,8 @@ public class ReducingDishes {
         System.out.println(maxSatisfaction(satisfaction));
     }
 
+    // TC: O(n * n)
+    // SC: O(n * n) + O(n * n)
     public static int maxSatisfaction(int[] satisfaction) {
         int n = satisfaction.length;
         Arrays.sort(satisfaction);
@@ -39,4 +41,54 @@ public class ReducingDishes {
 
         return dp[idx][t] = Math.max(take, notTake);
     }
+
+
+    // TC: O(n * n)
+    // SC: O(n * n)
+    /* public int maxSatisfaction(int[] satisfaction) {
+        int n = satisfaction.length;
+        Arrays.sort(satisfaction);
+
+        int[][] dp = new int[n + 1][n + 2];
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int t = 1;  t <= n; t++) {
+                // take
+                int take = (satisfaction[idx] * t) + dp[idx + 1][t + 1];
+
+                // not take
+                int notTake = dp[idx + 1][t];
+
+                dp[idx][t] = Math.max(take, notTake);
+            }
+        }
+
+        return dp[0][1];
+    } */
+
+
+    // TC: O(n * n)
+    // SC: O(2 * n)
+    /* public int maxSatisfaction(int[] satisfaction) {
+        int n = satisfaction.length;
+        Arrays.sort(satisfaction);
+        
+        int[] front = new int[n + 2];
+        
+        for (int idx = n - 1; idx >= 0; idx--) {
+            int[] curr = new int[n + 2];
+            for (int t = 1;  t <= n; t++) {
+                // take
+                int take = (satisfaction[idx] * t) + front[t + 1];
+
+                // not take
+                int notTake = front[t];
+
+                curr[t] = Math.max(take, notTake);
+            }
+            front = curr;
+        }
+        
+        return front[1];
+    } */
 }
