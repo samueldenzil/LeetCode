6a85ba9ba85ed1ddadf296c6f9b48d1f19301fc6
samueldenzil <denzil.s.samuel@gmail.com>
2023-03-31 07:15:28 +0530

    March 30, 2023

---
+++ All_Problems/MinimumCostForTickets.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/minimum-cost-for-tickets/
+package All_Problems;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
+public class MinimumCostForTickets {
+
+    public static void main(String[] args) {
+        int[] days = {1, 4, 6, 7, 8, 20};
+        int[] costs = {2, 7, 15};
+        System.out.println(mincostTickets(days, costs));
+    }
+
+    public static int mincostTickets(int[] days, int[] costs) {
+        Set<Integer> daySet = new HashSet<>();
+        for (int day : days) {
+            daySet.add(day);
+        }
+
+        int[] dp = new int[366];
+        Arrays.fill(dp, -1);
+
+        return helper(1, costs, daySet, dp);
+    }
+
+    private static int helper(int day, int[] costs, Set<Integer> daySet, int[] dp) {
+        if (day > 365) {
+            return 0;
+        }
+
+        if (dp[day] != -1) {
+            return dp[day];
+        }
+
+        int ans;
+        if (daySet.contains(day)) {
+            ans = Math.min(costs[0] + helper(day + 1, costs, daySet, dp), costs[1] + helper(day + 7, costs, daySet, dp));
+            ans = Math.min(ans, costs[2] + helper(day + 30, costs, daySet, dp));
+        } else {
+            ans = helper(day + 1, costs, daySet, dp);
+        }
+
+        return dp[day] = ans;
+    }
+}
---
+++ Dynamic_Programming/ScrambleString.java
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/scramble-string/
+package Dynamic_Programming;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class ScrambleString {
+
+    public static void main(String[] args) {
+        String s1 = "great", s2 = "rgeat";
+        System.out.println(isScramble(s1, s2));
+    }
+
+    public static boolean isScramble(String s1, String s2) {
+        Map<String, Boolean> dp = new HashMap<>();
+        return helper(s1, s2, dp);
+    }
+
+    private static boolean helper(String s1, String s2, Map<String, Boolean> dp) {
+        // base case
+        if (s1.equals(s2)) {
+            return true;
+        }
+        if (s1.length() <= 1 || s2.length() <= 1) {
+            return false;
+        }
+
+        String key = s1 + "-" + s2;
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+
+        int n = s1.length();
+        boolean flag = false;
+
+        for (int i = 1; i <= n - 1; i++) {
+            boolean swap = helper(s1.substring(0, i), s2.substring(n - i), dp) && helper(s1.substring(i), s2.substring(0, n - i), dp);
+            if (swap) {
+                flag = true;
+                break;
+            }
+
+            boolean notSwap = helper(s1.substring(0, i), s2.substring(0, i), dp) && helper(s1.substring(i), s2.substring(i), dp);
+            if (notSwap) {
+                flag = true;
+                break;
+            }
+        }
+
+        dp.put(key, flag);
+        return flag;
+    }
+}
