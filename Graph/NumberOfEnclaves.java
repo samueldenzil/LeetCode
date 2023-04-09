35b73a9790b65b098c89631fda0f5d3b3a4df32c
samueldenzil <denzil.s.samuel@gmail.com>
2023-04-09 08:46:30 +0530

    April 7, 2023

--- Graph/NumberOfEnclaves.java
+++ Graph/NumberOfEnclaves.java
@@ -69,75 +69,28 @@ public class NumberOfEnclaves {
     }
 
     // BFS Solution
-    /* public static int numEnclaves(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        boolean[][] vis = new boolean[n][m];
-        Queue<Pair> queue = new LinkedList<>();
-
-        for (int j = 0; j < m; j++) {
-            if (grid[0][j] == 1 && !vis[0][j]) {
-                queue.add(new Pair(0, j));
-                vis[0][j] = true;
-            }
-            if (grid[n - 1][j] == 1 && !vis[n - 1][j]) {
-                queue.add(new Pair(n - 1, j));
-                vis[n - 1][j] = true;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (grid[i][0] == 1 && !vis[i][0]) {
-                queue.add(new Pair(i, 0));
-                vis[i][0] = true;
-            }
-            if (grid[i][m - 1] == 1 && !vis[i][m - 1]) {
-                queue.add(new Pair(i, m - 1));
-                vis[i][m - 1] = true;
-            }
-        }
-
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
+    /* private void bfs(int row, int col, int[][] grid, boolean[][] vis, int[] dx, int[] dy) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{row, col});
+        vis[row][col] = true;
 
         while (!queue.isEmpty()) {
-            int row = queue.peek().row;
-            int col = queue.peek().col;
-            queue.poll();
+            row = queue.peek()[0];
+            col = queue.poll()[1];
+            queue.peek();
 
             for (int i = 0; i < 4; i++) {
-                int nrow = row + dx[i];
-                int ncol = col + dy[i];
-
-                if (isValid(nrow, ncol, grid, vis, n, m)) {
-                    queue.add(new Pair(nrow, ncol));
-                    vis[nrow][ncol] = true;
+                int newRow = row + dx[i];
+                int newCol = col + dy[i];
+                if (isValid(newRow, newCol, grid, vis)) {
+                    queue.add(new int[]{newRow, newCol});
+                    vis[newRow][newCol] = true;
                 }
             }
         }
-
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && !vis[i][j]) {
-                    count++;
-                }
-            }
-        }
-        return count;
     } */
 
     private static boolean isValid(int row, int col, int[][] grid, boolean[][] vis, int n, int m) {
         return row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == 1 && !vis[row][col];
-    }
-
-    static class Pair {
-        int row;
-        int col;
-
-        Pair(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
     }
 }
