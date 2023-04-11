2e9a84c13955bd610858c6a5d9241dc987256568
samueldenzil <denzil.s.samuel@gmail.com>
2023-04-11 07:33:11 +0530

    April 10, 2023

--- All_Problems/ValidParentheses.java
+++ All_Problems/ValidParentheses.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 public class ValidParentheses {
 
     public static void main(String[] args) {
-        String s = "(])";
+        String s = "()[]{}";
         System.out.println(isValid(s));
     }
 
