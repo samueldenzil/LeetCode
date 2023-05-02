ea269df4c31362962380252745c7ff85c92dc249
samueldenzil <denzil.s.samuel@gmail.com>
2023-05-02 08:54:17 +0530

    May 1, 2023

---
+++ All_Problems/AverageSalaryExcludingTheMinimumAndMaximumSalary.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+package All_Problems;
+
+public class AverageSalaryExcludingTheMinimumAndMaximumSalary {
+
+    public static void main(String[] args) {
+        int[] salary = {4000, 3000, 1000, 2000};
+        System.out.println(average(salary));
+    }
+
+    public static double average(int[] salary) {
+        int minSalary = Integer.MAX_VALUE;
+        int maxSalary = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int it : salary) {
+            sum += it;
+            minSalary = Math.min(minSalary, it);
+            maxSalary = Math.max(maxSalary, it);
+        }
+
+        return (double) (sum - minSalary - maxSalary) / (double) (salary.length - 2);
+    }
+}
