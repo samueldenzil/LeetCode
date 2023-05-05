fabe8cf2f0d7ecc27f22d43ac4697119de266d01
samueldenzil <denzil.s.samuel@gmail.com>
2023-05-05 15:10:27 +0530

    May 4, 2023

---
+++ Greedy/Dota2Senate.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/dota2-senate/
+package Greedy;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Dota2Senate {
+
+    public static void main(String[] args) {
+        String senate = "RD";
+        System.out.println(predictPartyVictory(senate));
+    }
+
+    public static String predictPartyVictory(String senate) {
+        int n = senate.length();
+        Queue<Integer> radiantQueue = new LinkedList<>();
+        Queue<Integer> direQueue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                radiantQueue.add(i);
+            } else {
+                direQueue.add(i);
+            }
+        }
+
+        while (!radiantQueue.isEmpty() && !direQueue.isEmpty()) {
+            int rIdx = radiantQueue.poll();
+            int dIdx = direQueue.poll();
+            if (rIdx < dIdx) {
+                radiantQueue.add(rIdx + n);
+            } else {
+                direQueue.add(dIdx + n);
+            }
+        }
+
+        return radiantQueue.size() > direQueue.size() ? "Radiant" : "Dire";
+    }
+}
