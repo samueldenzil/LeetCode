592fcd57dff4d9003952112b4e79810648078d45
samueldenzil <denzil.s.samuel@gmail.com>
2023-05-06 07:53:42 +0530

    May 5, 2023

---
+++ All_Problems/MaximumNumberOfVowelsInASubstringOfGivenLength.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+package All_Problems;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
+public class MaximumNumberOfVowelsInASubstringOfGivenLength {
+
+    public static void main(String[] args) {
+        String s = "abciiidef";
+        int k = 3;
+        System.out.println(maxVowels(s, k));
+    }
+
+    public static int maxVowels(String s, int k) {
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            if (vowels.contains(s.charAt(i))) {
+                count++;
+            }
+        }
+
+        int ans = count;
+        for (int i = k; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i - k))) {
+                count--;
+            }
+            if (vowels.contains(s.charAt(i))) {
+                count++;
+            }
+            ans = Math.max(ans, count);
+        }
+
+        return ans;
+    }
+}
