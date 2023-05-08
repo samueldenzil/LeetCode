fb4ed065842c7f268d71fbc38d7261152f9245b8
samueldenzil <denzil.s.samuel@gmail.com>
2023-05-08 07:41:53 +0530

    May 7, 2023

--- Arrays/MatrixDiagonalSum.java
+++ Arrays/MatrixDiagonalSum.java
@@ -1,16 +1,30 @@
+// https://leetcode.com/problems/matrix-diagonal-sum/
 package Arrays;
 
-class MatrixDiagonalSum {
-    public int diagonalSum(int[][] mat) {
+public class MatrixDiagonalSum {
+
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        System.out.println(diagonalSum(mat));
+    }
+
+    public static int diagonalSum(int[][] mat) {
+        int n = mat.length;
         int sum = 0;
-        for (int i = 0; i < mat.length; i++) {
-            sum = sum + mat[i][i];
-            sum = sum + mat[i][mat.length - i - 1];
+
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n - i - 1];
         }
-        if (mat.length % 2 != 0) {
-            sum = sum - mat[mat.length / 2][mat.length / 2];
+
+        if (n % 2 != 0) {
+            sum -= mat[n / 2][n / 2];
         }
+
         return sum;
     }
 }
-
