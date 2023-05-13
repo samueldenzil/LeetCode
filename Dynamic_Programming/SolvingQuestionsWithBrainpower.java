1168907b03ddaa8ff8c0c0d72b9ac746c68d03f6
samueldenzil <denzil.s.samuel@gmail.com>
2023-05-13 12:05:05 +0530

    May 12, 2023

---
+++ Dynamic_Programming/SolvingQuestionsWithBrainpower.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/solving-questions-with-brainpower/
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class SolvingQuestionsWithBrainpower {
+
+    public static void main(String[] args) {
+        int[][] questions = {{3, 2}, {4, 3}, {4, 4}, {2, 5}};
+        System.out.println(mostPoints(questions));
+    }
+
+    public static long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n];
+        Arrays.fill(dp, -1);
+        return helper(0, questions, dp);
+    }
+
+    private static long helper(int idx, int[][] questions, long[] dp) {
+        if (idx >= questions.length) {
+            return 0;
+        }
+
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+
+        // solve this question
+        long solve = questions[idx][0] + helper(idx + questions[idx][1] + 1, questions, dp);
+
+        // skip this question
+        long skip = helper(idx + 1, questions, dp);
+
+        return dp[idx] = Math.max(solve, skip);
+    }
+}
