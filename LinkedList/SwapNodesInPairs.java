84d4563169dd54cb8015b71a74ad5e944fa31abe
samueldenzil <denzil.s.samuel@gmail.com>
2023-05-17 07:30:13 +0530

    May 16, 2023

---
+++ LinkedList/SwapNodesInPairs.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+package LinkedList;
+
+public class SwapNodesInPairs {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        ListNode head = generateLinkedList(arr);
+
+        ListNode ans = swapPairs(head);
+        while (ans != null) {
+            System.out.print(ans.val + " -> ");
+            ans = ans.next;
+        }
+        System.out.println("null");
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode temp = head.next;
+        head.next = swapPairs(head.next.next);
+        temp.next = head;
+
+        return temp;
+    }
+
+    private static ListNode generateLinkedList(int[] arr) {
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        for (int it : arr) {
+            temp.next = new ListNode(it);
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
